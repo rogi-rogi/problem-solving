2dc3dd31d9bfdd585d13b383f35d67250c8e9334
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-02-02 14:47:13 +0900

    Create 14503.py

---
+++ algorithm/implementation/14503.py
@@ -0,0 +1,30 @@
+from sys import stdin
+N, M = map(int, stdin.readline().split())
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+X, Y, DIR = map(int , stdin.readline().split())
+board = [list(map(int, stdin.readline().split())) for _ in range(N)]
+cnt = 0
+
+def clean(X, Y, D):
+    global cnt
+    if board[X][Y] == 0:
+        board[X][Y] = -1
+        cnt += 1
+    for _ in range(4):
+        nd = (D + 3) % 4
+        nx = X + dx[nd]
+        ny = Y + dy[nd]
+        if (0 <= nx < N and 0 <= ny < M and board[nx][ny] == 0):
+            clean(nx, ny, nd)
+            return
+        D = nd
+    nx = X - dx[D]
+    ny = Y - dy[D]
+    if 0 <= nx < N and 0 <= ny < M:
+        if board[nx][ny] == 1:
+            print(cnt)
+            return
+        clean(nx, ny, D)
+        
+clean(X, Y, DIR)
