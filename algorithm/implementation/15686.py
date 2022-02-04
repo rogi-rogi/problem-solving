8d0f551729212970166ec5840eec4130e821e466
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-02-04 19:37:17 +0900

    Create 15686.py

---
+++ algorithm/implementation/15686.py
@@ -0,0 +1,21 @@
+from sys import stdin
+from itertools import combinations
+
+N, M = map(int, stdin.readline().split())
+city = [list(map(int, stdin.readline().split())) for _ in range(N)]
+houses, chicken = [], []
+
+for i in range(N) :
+    for j in range(N) :
+        if   city[i][j] == 1 :  houses.append((i, j))
+        elif city[i][j] == 2 : chicken.append((i, j))
+        
+min_distance = float('inf')
+for location in combinations(chicken, M) :
+    sum_distance = 0
+    for house in houses :
+        sum_distance += min([abs(house[0]-temp[0]) + abs(house[1]-temp[1]) for temp in location])
+        if min_distance <= sum_distance : break
+    if sum_distance < min_distance : min_distance = sum_distance
+        
+print(min_distance)
