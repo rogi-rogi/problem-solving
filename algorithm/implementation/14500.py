0a1f57c15e4622845c115e84523bc276807b3ef5
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-02-05 10:35:41 +0900

    Create 14500.py

---
+++ algorithm/implementation/14500.py
@@ -0,0 +1,32 @@
+from sys import stdin
+N, M = map(int, stdin.readline().split())
+board = [list(map(int, stdin.readline().split())) for _ in range(N)]
+visited = [[0] * M for _ in range(N)]
+directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+
+result = 0
+max_block_val = max(map(max, board))
+def dfs(X, Y, total, block) :
+    if block == 4 :
+        global result
+        result = max(result, total)
+        return
+    elif total + max_block_val * (4 - block) < result :
+        return
+    for direction in directions :
+        nx, ny = X + direction[0], Y + direction[1]
+        if 0 <= nx < N and 0 <= ny < M and visited[nx][ny] == 0 :
+            if block == 2 :
+                visited[nx][ny] = 1
+                dfs(X, Y, board[nx][ny] + total, block + 1)
+                visited[nx][ny] = 0
+            visited[nx][ny] = 1
+            dfs(nx, ny, board[nx][ny] + total, block + 1)
+            visited[nx][ny] = 0
+            
+for i in range(N) :
+    for j in range(M) :
+        visited[i][j] = 1
+        dfs(i, j, board[i][j], 1)
+        visited[i][j] = 0
+print(result)
