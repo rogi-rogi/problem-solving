9f03dd468e94ad7da70eae5973c2654013ffe493
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-02-09 19:49:29 +0900

    Update __17144.py

--- algorithm/__17144.py
+++ algorithm/__17144.py
@@ -8,35 +8,49 @@ if __name__ == "__main__" :
     clean_bot = []
     for row in range(N) :
         if board[row][0] == -1 :
-            clean_bot.append(row)
+            clean_bot.extend([row, row + 1])
+            break
     sec = 0
+    
     while True :
         temp = [[0] * M for _ in range(N)]
-        for i in range(N) :
-            for j in range(M) :
-                if board[i][j] > 4 :
+        for x in range(N) :
+            for y in range(M) :
+                if board[x][y] > 4 :
                     cnt = 0
                     for d in range(4) :
-                        nx = i + dx[d]
-                        ny = i + dy[d]
+                        nx = x + dx[d]
+                        ny = y + dy[d]
                         if 0 <= nx < N and 0 <= ny < M and board[nx][ny] != -1 :
-                            temp[nx][ny] += board[i][j] // 5
+                            temp[nx][ny] += board[x][y] // 5
                             cnt += 1
-                        temp[i][j] += board[i][j] - (board[i][j] // 5) * cnt
+                    temp[x][y] += board[x][y] - (board[x][y] // 5) * cnt
+                else : temp[x][y] += board[x][y]
         board = temp[:]
-        for i in range(1, -2, -2) :
-            d = 0 if i == 1 else 2
+        for i in range(N) :
+            print(board[i])
+        for i in range(2) :
+            d = 0 if i == 0 else 2
             x = clean_bot[i]
             y = 0
             while True :
                 nx = x + dx[d]
                 ny = y + dy[d]
-                if 0 <= nx < N and 0 <= ny < M :
-                    if board[nx][ny] == -1 : 
-                        break
+                if 0 <= nx < N and 0
+
+
+
+                <= ny < clean_bot[i] + 1 :
+                    if board[nx][ny] == -1 : break
                     if board[x][y] == -1 : board[nx][ny] = 0
                     else : board[x][y] = board[nx][ny]
-                else : d = (d + 4 + i) % 4
+                    x = nx
+                    y = ny
+                else :
+                    d = (d + 1)%4 if i == 0 else (d + 3) % 4
+                    print(x, y, d)
+                    x = x + dx[d]
+                    y = y + dy[d]
         sec += 1
         if sec == T :
             total = 0
@@ -44,3 +58,4 @@ if __name__ == "__main__" :
                 total += sum(board[i])
             print(total)
             break
+        
