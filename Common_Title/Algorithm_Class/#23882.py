240ae342e74484675e26b7e8cac8f9d6426b2693
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-02-12 14:55:45 +0900

    Update #23882.py

--- Common_Title/Algorithm_Class/#23882.py
+++ Common_Title/Algorithm_Class/#23882.py
@@ -3,7 +3,6 @@ arr = list(map(int, input().split()))
 change = 0
 for i in range(N, 1, -1) :
     max_idx = arr.index(max(arr[:i]))
-    
     if max_idx != i - 1 :
         change += 1
         arr[max_idx], arr[i - 1] = arr[i - 1], arr[max_idx]
