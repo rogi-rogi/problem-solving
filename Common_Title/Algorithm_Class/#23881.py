546ebf130c914e1ce32ac86b026df1f8db428294
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-02-12 15:13:44 +0900

    Update #23881.py

--- Common_Title/Algorithm_Class/#23881.py
+++ Common_Title/Algorithm_Class/#23881.py
@@ -10,4 +10,4 @@ for i in range(N, 1, -1) :
             print(arr[i - 1], arr[max_idx])
             exit(0)
         arr[max_idx], arr[i - 1] = arr[i - 1], arr[max_idx], 
-if change < K : print(-1)
+print(-1)
