f56f566637c7e243a225bc1775343ec3023d9ddf
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-02-12 15:29:26 +0900

    Update #23969.py

--- Common_Title/Algorithm_Class/#23969.py
+++ Common_Title/Algorithm_Class/#23969.py
@@ -7,7 +7,7 @@ for i in range(N, 0, -1) :
         if arr[j] > arr[j + 1] :
             change += 1
             arr[j], arr[j + 1] = arr[j + 1], arr[j]
-        if change == K :
-            for val in arr : print(val, end = ' ')
-            exit(0)
+            if change == K :
+                for val in arr : print(val, end = ' ')
+                exit(0)
 if change < K : print(-1)
