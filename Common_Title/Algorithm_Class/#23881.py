e0f8e237601ef4d805d4e3c5656d0139df4160a1
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-02-12 12:58:00 +0900

    Create #23881.py

---
+++ Common_Title/Algorithm_Class/#23881.py
@@ -0,0 +1,14 @@
+N, K = map(int, input().split())
+arr = list(map(int, input().split()))
+change = 0
+for i in range(N, 1, -1) :
+    max_idx = arr.index(max(arr[:i]))
+    
+    if max_idx != i - 1 :
+        change += 1
+        if change == K :
+            print(arr[i - 1], arr[max_idx])
+            exit(0)
+        arr[max_idx], arr[i - 1] = arr[i - 1], arr[max_idx],
+        
+if change < K : print(-1)
