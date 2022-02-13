0b733d14c09879911689ed42b05564cce24884b3
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-02-13 16:40:14 +0900

    Create #1759.py

---
+++ algorithm/#1759.py
@@ -0,0 +1,12 @@
+from itertools import combinations
+
+if __name__ == "__main__" : 
+    L, C = map(int, input().split())
+    for word in list(combinations(sorted(list(input().split())), L)) : 
+        vowel = consonant = 0
+        for char in word :
+            if char in "aeiou" : vowel += 1
+            else : consonant += 1
+            if vowel > 0 and consonant > 1 :
+                print("".join(word))
+                break
