64adffb74c5651cae0bb48cffbcb4048bde6a0e4
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-02-15 20:06:17 +0900

    Update 24053.py

--- Common_Title/Algorithm_Class/24053.py
+++ Common_Title/Algorithm_Class/24053.py
@@ -6,15 +6,14 @@ if A == B : print(1)
 else :
     for i in range(1, N) :
         temp = A[i]
-        j = i - 1
-        while A[j] > temp and j >= 0 :
-            A[j + 1] = A[j]
-            if A[j] == B[j] and B[j] == B[j + 1] and A == B :
-                print(1)
-                exit(0)
-            j -= 1
-        if j != i - 1 :
-            A[j + 1] = temp
+        if A[i - 1] > temp :
+            while A[i - 1] > temp and i >= 1 :
+                A[i] = A[i - 1]
+                if A[i - 1] == B[i - 1] and B[i - 1] == B[i] and A == B :
+                    print(1)
+                    exit(0)
+                i -= 1
+            A[i] = temp
             if A == B: 
                 print(1)
                 exit(0)
