1bc2d3573b5fb5b943d742e88036ef6c5459238c
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-02-16 19:09:41 +0900

    Create 23832.py

---
+++ algorithm/23832.py
@@ -0,0 +1,14 @@
+def EPF(N) :
+    euler = N
+    p = 2
+    while p * p <= N :
+        if N % p == 0 : euler = euler // p * (p - 1)
+        while N % p == 0 : N = N // p
+        p += 1
+    return euler if N == 1 else euler // N * (N - 1)
+
+if __name__ == "__main__" :
+    total = 0
+    for i in range(2, int(input()) + 1) :
+        total += EPF(i)
+    print(total)
