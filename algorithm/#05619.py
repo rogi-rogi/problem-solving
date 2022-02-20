547f275e1a7286202194b837eec796c4412737f9
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-02-20 15:49:08 +0900

    Create #05619.py

---
+++ algorithm/#05619.py
@@ -0,0 +1,12 @@
+from sys import stdin
+
+if __name__ == "__main__" :
+    n = int(stdin.readline())
+    a = sorted([int(stdin.readline().rstrip()) for _ in range(n)])
+    a = a[:4]
+    nums = []
+    for num in a :
+        for other_num in a :
+            if num != other_num :
+                nums.append(int(str(num) + str(other_num)))
+    print(sorted(nums)[2])
