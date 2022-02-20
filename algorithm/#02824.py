2b44270873d138c19a6d0bc8980f24d651361a3a
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-02-20 17:06:28 +0900

    Update #02824.py

--- algorithm/#02824.py
+++ algorithm/#02824.py
@@ -3,11 +3,11 @@ from math import gcd
 input = stdin.readline
 
 def list_mul(input_list) :
-    return eval('*'.join(list(str(i) for i in input_list)))
+    return int(eval('*'.join([i for i in input_list])))
     
 if __name__ == "__main__" : 
     N = input()
-    mul_n_list = list_mul(list(map(int, input().split())))
+    mul_n_list = list_mul(list(map(str, input().split())))
     M = input()
-    mul_m_list = list_mul(list(map(int, input().split())))
+    mul_m_list = list_mul(list(map(str, input().split())))
     print(str(gcd(mul_n_list, mul_m_list))[-9:])
