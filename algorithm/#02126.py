5e6d44fe7b9489571b9889e387cb61ffeddacf19
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-02-26 13:24:34 +0900

    Create #02126.py

---
+++ algorithm/#02126.py
@@ -0,0 +1,8 @@
+from collections import deque
+
+N = int(input())
+queue = deque([i for i in range(1, N + 1)])
+while len(queue) > 1 :
+    print(queue.popleft(), end = ' ')
+    queue.append(queue.popleft())
+print(queue.popleft())
