37ec21c822f283272906f32fb6e0d94b8901ad23
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-03-18 20:01:08 +0900

    Update #01715.py

--- algorithm/#01715.py
+++ algorithm/#01715.py
@@ -5,12 +5,10 @@ input = stdin.readline
 if __name__ == "__main__" :
     N = int(input())
     nums = [int(input()) for _ in range(N)]
-    if N == 1 : print(0)
-    else :
-        result = 0
-        heapq.heapify(nums)
-        while len(nums) > 1 :
-            sum_num = heapq.heappop(nums) + heapq.heappop(nums)
-            result += sum_num
-            heapq.heappush(nums, sum_num)
-        print(result)
+    result = 0
+    heapq.heapify(nums)
+    while len(nums) > 1 :
+        sum_num = heapq.heappop(nums) + heapq.heappop(nums)
+        result += sum_num
+        heapq.heappush(nums, sum_num)
+    print(result)
