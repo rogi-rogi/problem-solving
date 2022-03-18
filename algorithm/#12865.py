efa16164248dcadccb2accc5f1aa6be059775a04
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-03-18 17:40:59 +0900

    Update #12865.py

--- algorithm/#12865.py
+++ algorithm/#12865.py
@@ -12,4 +12,19 @@ if __name__ == "__main__" :
             else : dp[i][j] = max(dp[i - 1][j], V + dp[i - 1][j - W])
     print(dp[N][K])
     
-   
+#fast ver
+from sys import stdin
+input = stdin.readline
+
+if __name__ == "__main__" :
+    N, K = map(int, input().split())
+    dp = {0 : 0}
+    for _ in range(N) :
+        W, V = map(int, input().split())
+        temp = dict()
+        for v, w in dp.items() :
+            nv, nw = V + v, W + w
+            if nw < dp.get(nv, K + 1) :
+                temp[nv] = nw
+        dp.update(temp)
+    print(max(dp.keys()))
