64449671086584761a3f5c0957895faa0a408210
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-03-26 15:53:49 +0900

    Update 03190.py

--- algorithm/03190.py
+++ algorithm/03190.py
@@ -1,37 +1,39 @@
 from collections import deque
 from sys import stdin
+input = stdin.readline
 
-if __name__ == "__main__" :
-    N = int(stdin.readline())
-    K = int(stdin.readline())
-    board = [[0] * N for _ in range(N)]
-    for _ in range(K) :
-        x, y = map(int, stdin.readline().split())
-        board[x - 1][y - 1] = 1
-    turns = dict()
-    for _ in range(int(stdin.readline())) :
-        X, C = stdin.readline().split()
-        turns[int(X)] = C
-        
-    dx = [-1, 0, 1, 0]
-    dy = [0, 1, 0, -1]
-    direction = 1
+def isAreaOver(x, y) :
+    return not (0 <= x < N and 0 <= y < N)
+    
+def move() :
+    d = 1
     sec = 1
     x = y = 0
     visited = deque([(x,y)])
     board[x][y] = -1
     while True :
-        x = x + dx[direction]
-        y = y + dy[direction]
-        if 0 <= x < N and 0 <= y < N and board[x][y] != -1 :
-            if board[x][y] == 0 :
+        x = x + D[d][0]
+        y = y + D[d][1]
+        if not isAreaOver(x, y) and board[x][y] != -1 :
+            if board[x][y] == 0 :   # no apple
                 tail_x, tail_y = visited.popleft()
                 board[tail_x][tail_y] = 0
             board[x][y] = -1
             visited.append((x,y))
             if sec in turns.keys() :
-                direction = (direction + 3) % 4 if turns[sec] == 'L' else (direction + 1) % 4
+                d = (d + 3) % 4 if turns[sec] == 'L' else (d + 1) % 4
             sec += 1
-        else :
-            print(sec)
-            break
+        else : return sec
+            
+if __name__ == "__main__" :
+    N = int(input())
+    board = [[0] * N for _ in range(N)]
+    for _ in range(int(input())) :
+        x, y = map(int, input().split())
+        board[x - 1][y - 1] = 1
+    turns = dict()
+    for _ in range(int(input())) :
+        X, C = input().split()
+        turns[int(X)] = C
+    D = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+    print(move())
