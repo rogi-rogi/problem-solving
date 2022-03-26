b965d47d80c002373c85b3f2d1ec58cd19c7fdbd
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-03-26 16:28:29 +0900

    Update 14500.py

--- algorithm/14500.py
+++ algorithm/14500.py
@@ -1,34 +1,33 @@
 from sys import stdin
+input = stdin.readline
 
-def dfs(X, Y, total, block) :
+def isInArea(x, y) :
+    return (0 <= x < N and 0 <= y < M)
+
+def _dfs(x, y, nx, ny, total, block) :
+    visited[nx][ny] = 1
+    dfs(x, y, total, block)
+    visited[nx][ny] = 0
+    
+def dfs(x, y, total, block) :
     if block == 4 :
         global result
         result = max(result, total)
-        return
-    elif total + max_block_val * (4 - block) < result :
-        return
-    for direction in directions :
-        nx = X + direction[0]
-        ny = Y + direction[1]
-        if 0 <= nx < N and 0 <= ny < M and visited[nx][ny] == 0 :
-            if block == 2 :
-                visited[nx][ny] = 1
-                dfs(X, Y, board[nx][ny] + total, block + 1)
-                visited[nx][ny] = 0
-            visited[nx][ny] = 1
-            dfs(nx, ny, board[nx][ny] + total, block + 1)
-            visited[nx][ny] = 0
+    elif total + max_block_val * (4 - block) >= result :
+        for d in [(-1, 0), (0, 1), (1, 0), (0, -1)] :
+            nx, ny = x + d[0], y + d[1]
+            if isInArea(nx, ny) and visited[nx][ny] == 0 :
+                if block == 2 :
+                    _dfs(x, y, nx, ny, board[nx][ny] + total, block + 1)
+                _dfs(nx, ny, nx, ny, board[nx][ny] + total, block + 1)
             
 if __name__ == "__main__" :
-    N, M = map(int, stdin.readline().split())
-    board = [list(map(int, stdin.readline().split())) for _ in range(N)]
+    N, M = map(int, input().split())
+    board = [list(map(int, input().split())) for _ in range(N)]
     visited = [[0] * M for _ in range(N)]
-    directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
     result = 0
     max_block_val = max(map(max, board))
     for i in range(N) :
         for j in range(M) :
-            visited[i][j] = 1
-            dfs(i, j, board[i][j], 1)
-            visited[i][j] = 0
+            _dfs(i, j, i, j, board[i][j], 1)
     print(result)
