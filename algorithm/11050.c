3002857d5d5799d18a8fc5dd95b9eb1e5805f90c
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-04-03 12:23:04 +0900

    Update 11050.c

--- algorithm/11050.c
+++ algorithm/11050.c
@@ -4,10 +4,10 @@ int main()
 {
     int N, K;
     scanf("%d %d", &N, &K);
-    int sub_fac_N = 1, sub_fac_K = 1;
+    int sub_fac_N = 1, fac_K = 1;
     for (int i = 0; i < K; i++){
         sub_fac_N *= (N - i);
-        sub_fac_K *= (K - i);
+        fac_K *= (K - i);
     }
-    printf("%d", sub_fac_N / sub_fac_K);
+    printf("%d", sub_fac_N / fac_K);
 }
