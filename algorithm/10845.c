9cfc2ff04aed5a86139214fe9f0a4da6ac52a9b0
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-04-03 10:39:28 +0900

    Update 10845.c

--- algorithm/10845.c
+++ algorithm/10845.c
@@ -2,28 +2,24 @@
 #include <string.h>
 #define MAX 10001
 
-int front = -1, rear = -1;
-int empty()
-{
-  return front == rear;
-}
+int queue[MAX], front = -1, rear = -1;
+int empty() { return front == rear; }
 
 int main()
 {
-  int queue[MAX], N, n;
-  char cmd[6];
-  scanf("%d", &N);
-  while (N--){
-    scanf("%s", cmd);
-		
-    if (!(strcmp(cmd, "push"))){
-      scanf("%d", &n);
-      queue[++front] = n;
+    int N, n;
+    char cmd[6];
+    scanf("%d", &N);
+    while (N--) {
+        scanf("%s", cmd);
+        if (!(strcmp(cmd, "push"))) {
+            scanf("%d", &n);
+            queue[++front] = n;
+        }
+        else if (!strcmp(cmd, "pop"))     printf("%d\n", empty() ? -1 : queue[++rear]);
+        else if (!strcmp(cmd, "size"))    printf("%d\n", front - rear);
+        else if (!strcmp(cmd, "empty"))   printf("%d\n", empty());
+        else if (!strcmp(cmd, "front")) printf("%d\n", empty() ? -1 : queue[rear + 1]);
+        else if (!strcmp(cmd, "back"))  printf("%d\n", empty() ? -1 : queue[front]);
     }
-    else if (!strcmp(cmd, "pop"))     printf("%d\n", empty() ? -1 : queue[++rear]);
-    else if (!strcmp(cmd, "size"))    printf("%d\n", front - rear);
-    else if (!strcmp(cmd, "empty"))   printf("%d\n", empty());
-    else if (!strcmp(cmd, "front")) printf("%d\n", empty() ? -1 : queue[rear + 1]);
-    else if (!strcmp(cmd, "back"))  printf("%d\n", empty() ? -1 : queue[front]);
-  }
 }
