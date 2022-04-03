8a90ad70e86fffed4b4d872ce876620210594000
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-04-03 10:40:21 +0900

    Update 10870.c

--- algorithm/10870.c
+++ algorithm/10870.c
@@ -2,15 +2,15 @@
 
 int fibo(int n)
 {
-  int fiboNum[2] = {0, 1};
-  for (int i = 1 ; i < n; i++)
-    fiboNum[(i + 1) % 2] = fiboNum[i % 2] + fiboNum[(i - 1) % 2];
-  return fiboNum[n % 2];
+    int fiboNum[2] = {0, 1};
+    for (int i = 1 ; i < n; ++i)
+        fiboNum[(i + 1) % 2] = fiboNum[i % 2] + fiboNum[(i - 1) % 2];
+    return fiboNum[n % 2];
 }
 
 int main()
 {
-  int n;
-  scanf("%d", &n);
-  printf("%d", fibo(n));
+    int n;
+    scanf("%d", &n);
+    printf("%d", fibo(n));
 }
