82f52865a7c2906a7cb44cace80064cd2ed9cb04
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-04-03 10:39:43 +0900

    Update 10845.c

--- algorithm/10845.c
+++ algorithm/10845.c
@@ -16,9 +16,9 @@ int main()
             scanf("%d", &n);
             queue[++front] = n;
         }
-        else if (!strcmp(cmd, "pop"))     printf("%d\n", empty() ? -1 : queue[++rear]);
-        else if (!strcmp(cmd, "size"))    printf("%d\n", front - rear);
-        else if (!strcmp(cmd, "empty"))   printf("%d\n", empty());
+        else if (!strcmp(cmd, "pop"))   printf("%d\n", empty() ? -1 : queue[++rear]);
+        else if (!strcmp(cmd, "size"))  printf("%d\n", front - rear);
+        else if (!strcmp(cmd, "empty")) printf("%d\n", empty());
         else if (!strcmp(cmd, "front")) printf("%d\n", empty() ? -1 : queue[rear + 1]);
         else if (!strcmp(cmd, "back"))  printf("%d\n", empty() ? -1 : queue[front]);
     }
