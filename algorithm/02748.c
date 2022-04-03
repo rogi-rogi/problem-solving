4255f87e0499b8df51a330db66095d4c0f999692
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-04-03 10:40:41 +0900

    Update 02748.c

--- algorithm/02748.c
+++ algorithm/02748.c
@@ -3,7 +3,7 @@
 long long fibo(int n)
 {
     long long fiboNum[2] = {0, 1};
-    for (int i = 1 ; i < n; i++)
+    for (int i = 1 ; i < n; ++i)
         fiboNum[(i + 1) % 2] = fiboNum[i % 2] + fiboNum[(i - 1) % 2];
     return fiboNum[n % 2];
 }
