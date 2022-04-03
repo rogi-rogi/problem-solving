8e3cdf7eb15a53ad3fa79a86442052dbbb6304b9
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-04-03 12:24:53 +0900

    Update 11050.c

--- Common_Title/binomial_coefficient/11050.c
+++ Common_Title/binomial_coefficient/11050.c
@@ -2,12 +2,12 @@
 
 int main()
 {
-  int N, K;
-  scanf("%d %d", &N, &K);
-  int  a = 1, b = 1;
-  for (int i = 0; i < K; ++i) {
-    a *= (N - i);
-    b *= (K - i);
-  }
-  printf("%d", a / b);
+    int N, K;
+    scanf("%d %d", &N, &K);
+    int sub_fac_N = 1, fac_K = 1;
+    for (int i = 0; i < K; i++){
+        sub_fac_N *= (N - i);
+        fac_K *= (K - i);
+    }
+    printf("%d", sub_fac_N / fac_K);
 }
