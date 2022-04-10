c39832a7b5c7312e17bc33d0dd9d1a21d82f412f
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-04-10 15:29:23 +0900

    Update 11758.py

--- algorithm/11758.py
+++ algorithm/11758.py
@@ -1,13 +1,10 @@
 def CCW(v1, v2, v3) :
-    return (v2[0] - v1[0]) * (v3[1] - v1[1]) - (v3[0] - v1[0]) * (v2[1] - v1[1])
+    result = (v2[0] - v1[0]) * (v3[1] - v1[1]) - (v3[0] - v1[0]) * (v2[1] - v1[1])
+    if result == 0 : return 0
+    return 1 if result > 0 else -1
 
 if __name__ == "__main__" :
     v1 = list(map(int, input().split()))
     v2 = list(map(int, input().split()))
     v3 = list(map(int, input().split()))
-    result = CCW(v1, v2, v3)
-    if result > 0 :     result = 1
-    elif result < 0 :   result = -1
-    else :              result = 0
-    print(result)
-    
+    print(CCW(v1, v2, v3))
