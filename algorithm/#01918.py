ff74fc7dca18d94420375335ac7de611faead6bf
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-04-20 16:22:48 +0900

    Create #01918.py

---
+++ algorithm/#01918.py
@@ -0,0 +1,31 @@
+def PIS(op) : # Priority in stack
+    res = -1
+    if op == '*' or op == '/' : res = 2
+    elif op == '+' or op == '-'  : res = 1
+    elif op == '(' : res = 0
+    return res
+
+def PIE(op) : # Priority in expression
+    res = -1
+    if op == '*' or op == '/' : res = 2
+    elif op == '+' or op == '-'  : res = 1
+    elif op == '(' : res = 4
+    return res
+    
+if __name__ == "__main__" :
+    res = []
+    stack = [] # input op
+    for i in list(input()):
+        if i.isalpha() : res.append(i)
+        elif i == ')' :
+            while stack[-1] != '(' :
+                res.append(stack.pop())
+            stack.pop()
+        else :
+            while stack and PIS(stack[-1]) >= PIE(i) :
+                res.append(stack.pop())
+                if not stack : break
+            stack.append(i)
+    while stack : res.append(stack.pop())
+    print(*res, sep = '')
+        
