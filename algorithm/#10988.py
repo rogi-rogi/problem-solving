edf9d82b0bb3470617a40dad57d2ee2aae6f8951
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-04-22 18:59:38 +0900

    Update #10988.py

--- algorithm/#10988.py
+++ algorithm/#10988.py
@@ -1,8 +1,7 @@
 word = input()
-size = len(word)
 palindrome = True
-for i in range(size // 2) :
-    if word[i] != word[size - 1 - i] :
+for i in range(len(word) // 2) :
+    if word[i] != word[-1 - i] :
         palindrome = False
         break
 print(1 if palindrome else 0)
