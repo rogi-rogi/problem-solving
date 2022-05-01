77778291a0e1751b6a5a8fe06089ab9c2d160212
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-05-01 23:53:40 +0900

    Create #01159.py

---
+++ algorithm/#01159.py
@@ -0,0 +1,10 @@
+alpha = [0] * 26
+for _ in range(int(input())) :
+    alpha[ord(input()[0]) - 97] += 1
+surrender = True
+res = ""
+for i in range(26) :
+    if alpha[i] >= 5 : 
+        surrender = False
+        res += chr(i + 97)
+print("PREDAJA" if surrender else res )
