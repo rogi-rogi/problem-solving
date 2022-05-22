982f67ca878be951b2bfd6f573d9fdbe18daa3c3
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-05-22 15:07:24 +0900

    Update #14565.py

--- algorithm/#14565.py
+++ algorithm/#14565.py
@@ -1,16 +1,14 @@
-def exGCD(a, b) : 
+def exEuclid(a, b) : 
     def onTimeGCD(a, b, q) : return (b, a - q * b)
     d0, d1 = a, b
-    s0, s1 = 1, 0
-    t0, t1 = 0, 1
+    t0, t1 = 1, 0
     while d1 :
         q = d0 // d1
         d0, d1 = onTimeGCD(d0, d1, q)
-        s0, s1 = onTimeGCD(s0, s1, q)
         t0, t1 = onTimeGCD(t0, t1, q)
-    return ((s0, t0), (s0 + b, t0 - a))[s0 <= 0] if d0 == 1 else (-1, -1)
+    return (t0 + [0, b][t0 <= 0]) if d0 == 1 else -1
     
 if __name__ == "__main__" :
     N, A = map(int, input().split())
-    C, K = exGCD(A, N)
+    C = exEuclid(A, N)
     print(N - A, C)
