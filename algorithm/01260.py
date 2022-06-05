e36a96ab070274625253cc78a35097b98e1c83e4
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-06-06 00:13:16 +0900

    Update 01260.py

--- algorithm/01260.py
+++ algorithm/01260.py
@@ -1,5 +1,6 @@
-from collections import deque
 # adjacency list ver
+from collections import deque
+
 def dfs(v) :
     path.append(v)
     visited[v] = True
@@ -16,8 +17,8 @@ def bfs(v) :
         for nv in graph[v] :
             if not visited[nv] :
                 path.append(nv)
-                visited[nv] = True
                 queue.append(nv)
+                visited[nv] = True
 
 N, M, V = map(int, input().split())
 graph = [[] for _ in range(N + 1)]
@@ -33,3 +34,40 @@ print(*path)
 path, visited = [], [False] * (N + 1)
 bfs(V)
 print(*path)
+
+'''
+# adjacency matrix ver
+from collections import deque
+
+def dfs(v) :
+    path.append(v)
+    visited[v] = True
+    for nv in range(1, N + 1) :
+        if not visited[nv] and graph[v][nv] :
+            dfs(nv)
+            
+def bfs(v) :
+    queue = deque([v])
+    visited[v] = True
+    path.append(v)
+    while queue :
+        v = queue.popleft()
+        for nv in range(1, N + 1) :
+            if not visited[nv] and graph[v][nv]:
+                visited[nv] = True
+                path.append(nv)
+                queue.append(nv)
+
+N, M, V = map(int, input().split())
+graph = [[0] * (N + 1) for _ in range(N + 1)]
+for _ in range(M) : 
+    v1, v2 = map(int, input().split())
+    graph[v1][v2] = graph[v2][v1] = 1
+    
+path, visited = [], [False] * (N + 1)
+dfs(V)
+print(*path)
+path, visited = [], [False] * (N + 1)
+bfs(V)
+print(*path)
+'''
