bf63e56c5553b252583dd4da2945a6b4a6bbe164
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-06-05 16:37:25 +0900

    Update #12851.py

--- algorithm/#12851.py
+++ algorithm/#12851.py
@@ -1,3 +1,4 @@
+#default ver. N -> K
 from collections import deque
 
 def bfs() :
@@ -22,3 +23,37 @@ if __name__ == "__main__" :
     MAX = 100001
     N, K = map(int, input().split())
     print(*bfs(), sep = '\n')
+
+'''
+#fast ver. K -> N
+from collections import deque
+
+def bfs() :
+    if N >= K : return N - K, 1
+    queue = deque([(K, 0)])
+    visited = [False] * MAX
+    cnt, std_time = 0, MAX
+    while queue :
+        x, time = queue.popleft();
+        visited[x] = True
+        if time > std_time : continue
+        if x == N :
+            if time < std_time :
+                std_time = time
+            cnt += 1
+        else :
+            for nx in [x - 1, x + 1] :
+                if 0 <= nx < MAX and not visited[nx] :
+                    if time + 1 <= std_time :
+                        queue.append((nx, time + 1))
+            nx = x // 2
+            if x % 2 == 0 and not visited[nx] :
+                if time + 1 <= std_time :
+                    queue.append((nx, time + 1))
+    return std_time, cnt
+
+if __name__ == "__main__" :
+    MAX = 100001
+    N, K = map(int, input().split())
+    print(*bfs(), sep = '\n')
+'''
