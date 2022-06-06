ce9061bdbd83f56948b73cc8397837a24a1d5c9d
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-06-06 17:04:25 +0900

    Update #01931.py

--- algorithm/#01931.py
+++ algorithm/#01931.py
@@ -6,9 +6,9 @@ rooms = [[*map(int, input().split())] for _ in range(N)]
 rooms.sort(key = lambda x : x[0])
 rooms.sort(key = lambda x : x[1])
 cnt, end = 0, 0
-for i in rooms :
-    if i[0] >= end :
-        end = i[1]
+for s, e in rooms :
+    if s >= end :
+        end = e
         cnt += 1
     else : continue
 print(cnt)
