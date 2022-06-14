ba102757c98ee1f78b6b45b4fbf77ce88d386b91
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-06-14 16:59:29 +0900

    Update #11003.py

--- algorithm/#11003.py
+++ algorithm/#11003.py
@@ -4,10 +4,10 @@ from collections import deque
 if __name__ == "__main__" :
     N, L = map(int, input().split())
     D = []
-    pq = deque()
+    dq = deque()
     for idx, a in enumerate([*map(int, input().split())]) :
-        while pq and pq[-1][1] > a : pq.pop()
-        while pq and idx - pq[0][0] >= L : pq.popleft()
-        pq.append((idx, a))
-        D.append(pq[0][1])
+        while dq and dq[-1][1] > a : dq.pop()
+        while dq and idx - dq[0][0] >= L : dq.popleft()
+        dq.append((idx, a))
+        D.append(dq[0][1])
     print(*D)
