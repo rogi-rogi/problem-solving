6ef1388d1c32b19a309650efde340758cd79224f
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-06-15 23:00:38 +0900

    Update ##05430.py

--- algorithm/##05430.py
+++ algorithm/##05430.py
@@ -2,23 +2,23 @@ from collections import deque
 
 if __name__ == "__main__" :
     for i in range(int(input())) :
-        P = [*input()]
+        P = [*input().replace("RR", '')]
         n = int(input())
         error = False
-        if n == 0 : 
+        if n == 0 :
             input()
             X = list()
-        else : X = deque([*map(int, input()[1:-1].split(','))])
+        else : X = deque([*input()[1:-1].split(',')])
         reverse = False
         for p in P :
             if p == 'R' :
                 reverse = not reverse
-                continue
-            if X :
-                if reverse : X.pop()
-                else : reverse : X.popleft()
             else :
-                error = True
-                break
-        print("error" if error else str([*X][::[1, -1][reverse]]).replace(' ', ''))
+                if X :
+                    if reverse : X.pop()
+                    else : X.popleft()
+                else :
+                    error = True
+                    break
+        print("error" if error else '[' + ','.join([*X][::[1, -1][reverse]]) + ']')
         
