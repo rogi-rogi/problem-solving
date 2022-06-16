7c8ac789be6056c22e586fac0931fed74b2fffe6
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-06-16 23:56:28 +0900

    Create ##3111.py

---
+++ algorithm/##3111.py
@@ -0,0 +1,23 @@
+from collections import deque 
+
+if __name__ == "__main__" :
+    A = input()
+    T = input()
+    A_first_chr = A[0]
+    A_last_chr = A[-1]
+    len_A = len(A)
+    queue = deque([*T])
+    left_stack, right_stack = [], []
+    reverse = False
+    while queue :
+        if reverse :
+            right_stack.append(queue.pop())
+            if right_stack[-1] == A_first_chr and ''.join(right_stack[-len_A:]) == A[::-1] :
+                del right_stack[-len_A:]
+                reverse = not reverse
+        else :
+            left_stack.append(queue.popleft())
+            if left_stack[-1] == A_last_chr and ''.join(left_stack[-len_A:]) == A :
+                del left_stack[-len_A:]
+                reverse = not reverse
+    print(*left_stack, *right_stack[::-1])
