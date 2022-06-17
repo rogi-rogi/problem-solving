6e2fb33a4524374c055efbadab4a8d109e25e2fa
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-06-18 00:49:08 +0900

    Create #03995.py

---
+++ algorithm/#03995.py
@@ -0,0 +1,28 @@
+from math import gcd
+
+def exEuclid(a, b) : 
+    def onTimeGCD(a, b, q) : return (b, a - q * b)
+    d0, d1 = a, b
+    t0, t1 = 1, 0
+    while d1 :
+        q = d0 // d1
+        d0, d1 = onTimeGCD(d0, d1, q)
+        t0, t1 = onTimeGCD(t0, t1, q)
+    return (t0 + [0, b][t0 <= 0]) if d0 == 1 else -1
+    
+if __name__ == "__main__" :
+    for _ in range(int(input())) :
+        K, C = map(int, input().split())
+        MAX = int(1e9)
+        possible = True
+        if gcd(K, C) != 1 : possible = False
+        else :
+            if C == 1 :
+                if K + 1 > MAX : possible = False
+                else :
+                    print(K + 1)
+                    continue
+            else :
+                X = exEuclid(C, K)
+                if X > MAX : possible = False
+        print("IMPOSSIBLE" if possible else X)
