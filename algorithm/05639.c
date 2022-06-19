8f20e82da1105a4aa4ffa8c28bf8ff5e001bfc1e
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-06-19 22:58:43 +0900

    Update 05639.c

--- algorithm/05639.c
+++ algorithm/05639.c
@@ -17,15 +17,15 @@ pNode make_tree(pNode root, int data)
 {
     if (root == NULL) {
         root = (pNode)malloc(sizeof(pNode));
-		root->data = data;
-		root->left = NULL;
-		root->right = NULL;
-		return root;
-	} else if (data < root->data)
-		root->left = make_tree(root->left, data);
-	else if (data > root->data)
-		root->right = make_tree(root->right, data);
-	return root;
+        root->data = data;
+        root->left = NULL;
+        root->right = NULL;
+        return root;
+    } else if (data < root->data)
+        root->left = make_tree(root->left, data);
+    else if (data > root->data)
+        root->right = make_tree(root->right, data);
+    return root;
 }
 
 int main()
