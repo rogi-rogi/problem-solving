0192da4b2299baed5e49414a49edd3bef42c1b68
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-06-19 19:49:38 +0900

    Update #15824.py

--- algorithm/#15824.py
+++ algorithm/#15824.py
@@ -4,5 +4,5 @@ if __name__ == "__main__" :
     MOD = int(1e9 + 7)
     res = 0
     for i in range(N) :
-        res += SHU_list[i] * (pow(2, i, MOD) - pow(2, N - i - 1, MOD)) % MOD
-    print((res + MOD) % MOD)
+        res = (res + SHU_list[i] * (pow(2, i, MOD) - pow(2, N - i - 1, MOD))) % MOD
+    print(res)
