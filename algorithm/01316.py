88f9c775c7df728dcc8ea86bbabee4e71fbbe8a6
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-06-22 23:32:31 +0900

    Update 01316.py

--- algorithm/01316.py
+++ algorithm/01316.py
@@ -1,14 +1,21 @@
-from sys import stdin
+cnt = 0
+for _ in range(int(input())) :
+    group = [False] * 26
+    word = input()
+    cnt += 1
+    group[ord(word[0]) - 97] = True
+    for idx in range(1, len(word)) :
+        idx_g = ord(word[idx]) - 97
+        if word[idx - 1] != word[idx] and group[idx_g] :
+            cnt -= 1
+            break
+        else : group[idx_g] = True
+print(cnt)
 
-if __name__ == "__main__" :
-    N = int(stdin.readline())
-    cnt = 0
-    isGroup = 1
-    for _ in range(N) :
-        word = stdin.readline()
-        for idx in range(1, len(word)-1) :
-            if word[idx - 1] != word[idx] and word[idx - 1] in word[idx:] :
-                isGroup = 0;
-        if isGroup == 1 :   cnt += 1
-        else :              isGroup = 1
-    print(cnt)
+# simple ver.
+cnt = 0
+for _ in range(int(input())):
+    word = input()
+    if list(word) == sorted(word, key=word.find):
+        cnt += 1
+print(result)
