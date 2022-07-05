0aa3001797b137562205d51c893e68e1f56d7b61
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-07-05 17:43:36 +0900

    Create _#13970.py

---
+++ algorithm/_#13970.py
@@ -0,0 +1,28 @@
+#...38%
+def EulerPhiList(n) :
+    eulerList = [*range(n + 1)]
+    for i in range(1, n) :
+        for j in range(i + i, n + 1, i) :
+                eulerList[j] -= eulerList[i]
+    return eulerList
+    
+def power_towers(X, mod, x_size) :
+    if mod == 1 or x_size == 0 : return 1
+    if x_size >= 1 :
+        x1 = X.pop(0)
+        if x_size >= 2 :
+            temp = pow(x1, power_towers(X, eulerList[mod], x_size - 1), mod)
+            return temp + [0, eulerList[mod]][temp >= mod]
+        return x1
+    
+if __name__ == "__main__" :
+    T, M = map(int, input().split())
+    eulerList = EulerPhiList(M)
+    for _ in range(T) :
+        X = [*map(int, input().split())]
+        x_size = X.pop(0)
+        x1 = X.pop(0)
+        if X :
+            print(pow(x1, power_towers(X, eulerList[M], x_size - 1), M))
+        else : print(x1 % M)
+        
