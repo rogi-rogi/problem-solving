08d1fd24279a7451d38ca08c2b058c2b15a3593a
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-07-31 15:30:26 +0900

    Update #02530.py

--- algorithm/#02530.py
+++ algorithm/#02530.py
@@ -1,8 +1,5 @@
 A, B, C = map(int, input().split())
 D = int(input())
 m = (C + D) // 60
-C = (C + D) % 60
 h = (B + m) // 60
-B = (B + m) % 60
-A = (A + h) % 24
-print(A, B, C)
+print((A + h)%24, (B + m)%60, (C + D)%60)
