da093beb308863f513a69b98f8165b23d5ba220d
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-08-03 23:03:41 +0900

    Update #05575.py

--- algorithm/#05575.py
+++ algorithm/#05575.py
@@ -1,4 +1,4 @@
 for _ in range(3) :
-    fh, fm, fs, lh, lm, ls = map(int, input().split())
-    t = (lm - fm) * 60 + (lh - fh) * 3600 + ls - fs
+    h1, m1, s1, h2, m2, s2 = map(int, input().split())
+    t = (m2 - m1) * 60 + (h2 - h1) * 3600 + s2 - s1
     print(t // 3600, (t % 3600) // 60, (t % 3600) % 60)
