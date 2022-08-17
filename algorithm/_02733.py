00c7794ba4edbce1df1a65072fee0409850fdbfc
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-08-17 23:44:25 +0900

    Create _02733.py

---
+++ algorithm/_02733.py
@@ -0,0 +1,32 @@
+from sys import stdin
+input = stdin.readline
+
+def bf_command(idx, ch) :
+    if ch == '>' : ptr = [ptr + 1, 0][ptr == 32767]
+    elif ch == '<' : ptr = [ptr - 1, 0][ptr == 0]
+    elif ch == '+' : arr[ptr] = [arr[ptr] + 1, 0][ptr == 255]
+    elif ch == '-' : arr[ptr] = [arr[ptr] - 1, 255][ptr == 0]
+    elif ch == '.' : res.append(chr(arr[ptr]))
+    elif ch == '[' : pass
+    
+if __name__ == "__main__" :
+    for i in range(int(input())) :
+        arr = [0] * 32768
+        ptr = 0
+        res = ["PROGRAM #%d:" % (i + 1)]
+        buff = []
+        move = dict()
+        while True :
+            line = input().replace(' ', '')
+            if line == "end" : break
+            idx = line.find('%')
+            buff.append([line, line[:idx]][idx != -1])
+        line = ''.join(buff)
+        temp = []
+        for idx1, ch in enumerate(line) :
+            if ch == '[' :
+                temp.append(idx1)
+            elif ch == ']' :
+                idx2 = temp.pop()
+                move[idx1], move[idx2] = idx2, idx1
+        
