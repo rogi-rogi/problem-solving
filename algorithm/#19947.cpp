dc7e341dbec49920400502e5b112960bb5e39507
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-09-04 23:40:58 +0900

    Create #19947.cpp

---
+++ algorithm/#19947.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+double dp[11];
+
+int main()
+{
+    int Y;
+    cin >> dp[0] >> Y;
+    for (int i = 1; i <= Y; ++i) {
+        dp[i] = (dp[i - 1] * 1.05);
+        if (i >= 3) dp[i] = max(dp[i], dp[i - 3] * 1.2);
+        if (i >= 5) dp[i] = max(dp[i], dp[i - 5] * 1.35);
+        dp[i] = (int)dp[i];
+    }
+    cout << dp[Y];
+}
