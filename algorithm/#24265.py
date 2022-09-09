817e3940f037d262bebb34dfbf486aff7b4d8910
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-09-09 14:53:47 +0900

    Add files via upload

---
+++ algorithm/#24265.py
@@ -0,0 +1,2 @@
+n = int(input())
+print(n * (n - 1) // 2, 2, sep = '\n')
---
+++ algorithm/#24267.py
@@ -0,0 +1,9 @@
+# abbreviated ver
+n = int(input())
+print(n*(n - 1)*(n - 2)//6,  3, sep = '\n')
+
+# view ver
+arr = [i for i in range(int(input()) - 1)]
+for i in range(1, len(arr)) :
+    arr[i] = arr[i-1] + arr[i]
+print(sum(arr), 3, sep = '\n')
