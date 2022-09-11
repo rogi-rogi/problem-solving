1dbe61da3846fc551a70327359ab30512fa32174
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-09-11 22:28:27 +0900

    Update #15489.cpp

--- algorithm/#15489.cpp
+++ algorithm/#15489.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
-#define SIZE 30
+#define SIZE 31
 
 using namespace std;
 
+int dp[SIZE][SIZE];
+
 int main()
 {
-    int dp[SIZE + 1][SIZE + 1];
-    for (int i = 1; i <= SIZE; ++i) dp[i][1] = dp[i][i] = 1;
-  /*
-    for (int i = 2; i < SIZE - 1; ++i)
+    for (int i = 1; i < SIZE; ++i) dp[i][1] = dp[i][i] = 1;
+    for (int i = 3; i < SIZE; ++i)
         for (int j = 2; j <= i; ++j)
             dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
-            */
     int R, C, W, sum = 0;
     cin >> R >> C >> W;
     for (int i = 0; i < W; ++i)
