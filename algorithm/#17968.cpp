f546eb760f4d721436008d0684f1a8c7de693846
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-09-12 07:18:08 +0900

    Update #17968.cpp

--- algorithm/#17968.cpp
+++ algorithm/#17968.cpp
@@ -8,18 +8,16 @@ int main()
 	int n;
 	cin >> n;
 	for (int i = 2; i <= n; ++i) {
-	    int flag = 0;
-		for (int val = 1; ; ++val) {
-		    flag = 0;
-		    A[i] = val;
-		    for (int k = 1; i - 2 * k >= 0; ++k) {
-		        if (A[i] - A[i - k] == A[i - k] - A[i - 2 * k]) {
-		            flag = 1;
-		            break;
-		        }
-		    }
-		    if (!flag) break;
-		}
+	    for (int val = 1, flag = 1; flag; ++val) {
+	        flag = 0;
+	        A[i] = val;
+	        for (int k = 1; i - 2 * k >= 0; ++k) {
+	            if (A[i] - A[i - k] == A[i - k] - A[i - 2 * k]) {
+	                flag = 1;
+	                break;
+	            }
+	        }
+	    }
 	}
 	cout << A[n];
 }
