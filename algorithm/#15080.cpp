88bdbf794f48b4aa7653ce06748c9a867e878871
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-09-14 22:43:11 +0900

    Update #15080.cpp

--- algorithm/#15080.cpp
+++ algorithm/#15080.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 int main()
 {
-    int t1[3], t2[3];
-    scanf("%d : %d : %d", &t1[0], &t1[1], &t1[2]);
-    scanf("%d : %d : %d", &t2[0], &t2[1], &t2[2]);
-    int start_time = t1[0] * 3600 + t1[1] * 60 + t1[2];
-    int end_time = t2[0] * 3600 + t2[1] * 60 + t2[2];
+    int t[3];
+    scanf("%d : %d : %d", &t[0], &t[1], &t[2]);
+    int start_time = t[0] * 3600 + t[1] * 60 + t[2];
+    scanf("%d : %d : %d", &t[0], &t[1], &t[2]);
+    int end_time = t[0] * 3600 + t[1] * 60 + t[2];
     cout << end_time - start_time + (start_time > end_time ? 3600 * 24 : 0);
 }
