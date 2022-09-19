b783cfa7ba6be2e74b2cf6246c7510b8d9fe5ac2
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-09-19 14:52:14 +0900

    Update 11053.c

--- algorithm/11053.c
+++ algorithm/11053.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#define max(a, b) (((a) > (b)) ? (a) : (b))
 
 int A[1000], dp[1000];
 
@@ -6,8 +7,7 @@ int LIS_length(int i)
 {
     int length = 0;
     for (int j = 0; j < i; ++j)
-        if (A[j] < A[i] && length < dp[j])
-            length = dp[j];
+        if (A[j] < A[i]) length = max(length, dp[j]);
     return length + 1;
 }
 
