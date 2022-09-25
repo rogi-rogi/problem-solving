cd2ba37f82a96a314c7e5e7890948b4379a782bd
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-09-25 22:52:52 +0900

    Update 16236.py

--- algorithm/16236.py
+++ algorithm/16236.py
@@ -2,7 +2,7 @@ from collections import deque
 
 def eatFish(x, y) :
     queue = deque([(x, y, 0)])      # x, y, time
-    move_list = []                  # recode all movement info
+    eat_fish = []                   # list of fish that sharks can eat
     visited = [[False] * N for _ in range(N)]
     visited[x][y] = True
     min_time = 400                  # 20 * 20
@@ -11,20 +11,20 @@ def eatFish(x, y) :
         for dx, dy in [(-1, 0), (0, 1), (1, 0), (0, -1)] :
             nx, ny = x + dx, y + dy
             if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny] :
-                if graph[nx][ny] <= shark_size :
+                if graph[nx][ny] <= shark_size :    # empty area or 
                     visited[nx][ny] = True
                     if 0 < graph[nx][ny] < shark_size :
                         min_time = time
-                        move_list.append((time + 1, nx, ny))
+                        eat_fish.append((time + 1, nx, ny))
                     if time <= min_time : queue.append((nx, ny, time + 1))
     # sorting criteria : (1) time(=distance), (2) x(=up), (3) y(=left)
-    return sorted(move_list)[0] if move_list else False
+    return sorted(eat_fish)[0] if eat_fish else False
                     
 if __name__ == "__main__" :
     N = int(input())
     graph = [[*map(int, input().split())] for _ in range(N)]
     shark_size, time = 2, 0
-    shark, fish = [], []
+    shark, fish = [], []                # recode shark/fish location
     for i in range(N) :
         for j in range(N) :
             if 1 <= graph[i][j] <= 6 : fish.append((i, j))
