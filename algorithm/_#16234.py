f7e7321d2e8d5f95cde5ccd0264938578fd2c7d3
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-09-27 23:57:11 +0900

    Update _#16234.py

--- algorithm/_#16234.py
+++ algorithm/_#16234.py
@@ -39,22 +39,22 @@ if __name__ == "__main__" :
         day += 1
     print(day)
 '''    
-def getUnitedInfo(x, y) : #return sum_population
-    pass
+
+def getUnitedInfo(x, y) :
     queue = deque([(x, y)])
     united_nations = [(x, y)]                   # maybe (x, y) is united nations
     sum_population = graph[x][y]
     while queue :
         x, y = queue.popleft()
         for nx, ny in [(x, y + 1), (x + 1, y), (x, y - 1), (x - 1, y)] :
-            if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny] :
+            if 0 <= nx < N and 0 <= ny < N and days[nx][ny] != day :
                 if L <= abs(graph[nx][ny] - graph[x][y]) <= R :
-                    visited[nx][ny] = True
+                    days[nx][ny] = day
                     united_nations.append((nx, ny))
                     sum_population += graph[nx][ny]
                     queue.append((nx, ny))
     return united_nations, sum_population
-
+    
 if __name__ == "__main__" :
     N, L, R = map(int, input().split())
     graph = [[*map(int, input().split())] for _ in range(N)]
@@ -65,10 +65,13 @@ if __name__ == "__main__" :
         for _ in range(len(nations)) :
             x, y = nations.popleft()
             if days[x][y] != day :
-                visited[x][y] = day
-                
-                
-                
-                # _, population =  getUnitedInfo(x, y)
-                
+                days[x][y] = day
+                united_nations, population =  getUnitedInfo(x, y)
+                cnt_nations = len(nations)
+                if cnt_nations > 1 :       # maybe united nation and real unsited nations ?
+                    population //= cnt_nations
+                    for x, y in united_nations :
+                        graph[x][y] = population
+                        
+        pass
     print(day)
