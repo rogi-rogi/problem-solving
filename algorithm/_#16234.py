624201e1e4ee21a9f3e0e8e9e3635a820e3c44b5
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-09-27 23:20:05 +0900

    Update _#16234.py

--- algorithm/_#16234.py
+++ algorithm/_#16234.py
@@ -3,7 +3,7 @@ from sys import stdin
 input = stdin.readline
 
 def getUnitedInfo(x, y) :
-    queue = deque((x, y))
+    queue = deque([(x, y)])
     united_nations = [(x, y)]                   # maybe (x, y) is united nations
     sum_population = graph[x][y]
     while queue :
@@ -21,19 +21,20 @@ if __name__ == "__main__" :
     N, L, R = map(int, input().split())
     graph = [[*map(int, input().split())] for _ in range(N)]
     day = 0
-    flag = True
-    while flag :
+    while True :
+        flag = False
         visited = [[False] * N for _ in range(N)]
         for i in range(N) :
             for j in range(N) :
                 if not visited[i][j] :
                     visited[i][j] = True
-                    pass
-                    nations, popilation = getUnitedInfo(i, j)
+                    nations, population = getUnitedInfo(i, j)
                     cnt_nations = len(nations)
                     if cnt_nations > 1 :       # maybe united nation and real unsited nations ?
-                        flag = False
+                        flag = True
+                        population //= cnt_nations
                         for x, y in nations :
-                            graph[x][y] = popilation // cnt_nations
+                            graph[x][y] = population
+        if not flag : break
+        day += 1
     print(day)
-        
