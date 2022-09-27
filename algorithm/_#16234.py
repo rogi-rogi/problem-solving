1c1ec3017b54cda69da05b58a0ed41e1b73fe64c
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-09-27 23:49:24 +0900

    Update _#16234.py

--- algorithm/_#16234.py
+++ algorithm/_#16234.py
@@ -1,7 +1,7 @@
 from collections import deque
 from sys import stdin
 input = stdin.readline
-
+'''
 def getUnitedInfo(x, y) :
     queue = deque([(x, y)])
     united_nations = [(x, y)]                   # maybe (x, y) is united nations
@@ -16,7 +16,7 @@ def getUnitedInfo(x, y) :
                     sum_population += graph[nx][ny]
                     queue.append((nx, ny))
     return united_nations, sum_population
-                    
+
 if __name__ == "__main__" :
     N, L, R = map(int, input().split())
     graph = [[*map(int, input().split())] for _ in range(N)]
@@ -37,4 +37,38 @@ if __name__ == "__main__" :
                             graph[x][y] = population
         if not flag : break
         day += 1
+    print(day)
+'''    
+def getUnitedInfo(x, y) : #return sum_population
+    pass
+    queue = deque([(x, y)])
+    united_nations = [(x, y)]                   # maybe (x, y) is united nations
+    sum_population = graph[x][y]
+    while queue :
+        x, y = queue.popleft()
+        for nx, ny in [(x, y + 1), (x + 1, y), (x, y - 1), (x - 1, y)] :
+            if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny] :
+                if L <= abs(graph[nx][ny] - graph[x][y]) <= R :
+                    visited[nx][ny] = True
+                    united_nations.append((nx, ny))
+                    sum_population += graph[nx][ny]
+                    queue.append((nx, ny))
+    return united_nations, sum_population
+
+if __name__ == "__main__" :
+    N, L, R = map(int, input().split())
+    graph = [[*map(int, input().split())] for _ in range(N)]
+    days = [[-1] * N for _ in range(N)]
+    nations = deque([(i, j) for i in range(N) for j in range(N)])
+    day = 0
+    while True :
+        for _ in range(len(nations)) :
+            x, y = nations.popleft()
+            if days[x][y] != day :
+                visited[x][y] = day
+                
+                
+                
+                # _, population =  getUnitedInfo(x, y)
+                
     print(day)
