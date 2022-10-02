13e03f2374f3f8b48f027c74c796e2a18b76b203
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-10-02 17:13:04 +0900

    Update #14425.cpp

--- algorithm/#14425.cpp
+++ algorithm/#14425.cpp
@@ -5,18 +5,18 @@ using namespace std;
 
 int main()
 {
-	int N, M;
-	cin >> N >> M;
-	string str;
-	map<string, bool> set_str;
-	for (int i = 0; i < N; ++i) {
-	    cin >> str;
-	    set_str[str] = true;
-	}
-	int cnt = 0;
-	for (int i = 0; i < M; ++i) {
-	    cin >> str;
-	    if (set_str[str]) ++cnt;
-	}
-	cout << cnt << '\n';
+    int N, M;
+    cin >> N >> M;
+    string str;
+    map<string, bool> set_str;
+    for (int i = 0; i < N; ++i) {
+        cin >> str;
+        set_str[str] = true;
+    }
+    int cnt = 0;
+    for (int i = 0; i < M; ++i) {
+        cin >> str;
+        if (set_str[str]) ++cnt;
+    
+    cout << cnt << '\n';
 }
