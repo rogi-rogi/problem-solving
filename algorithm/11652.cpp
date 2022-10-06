03d69d867ef5e8464f4c80d7ab6c7dc61c1baa50
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-10-06 17:52:29 +0900

    Update 11652.cpp

--- algorithm/11652.cpp
+++ algorithm/11652.cpp
@@ -14,7 +14,7 @@ int main()
     map<ll, int> cards;
     for (int i = 0; i < N; ++i) {
         cin >> num;
-        cards[num];
+        ++cards[num];
     } 
     for (map<ll, int>::iterator iter = cards.begin(); iter != cards.end(); ++iter) {
         if (cnt < iter->second) {
