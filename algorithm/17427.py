c363d5fc4cd9f491fc790b88a50a75e98433c258
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-10-09 10:51:37 +0900

    Update 17427.py

--- algorithm/17427.py
+++ algorithm/17427.py
@@ -1,7 +1,8 @@
 N = int(input())
 F = N
-for i in range(2, N // 2 + 1) :
-    F += N // i * i
-for i in range(N // 2 + 1, N + 1) :
-    F += i
+if N != 1 : 
+    for i in range(2, N//2 + 1) :
+        F += N // i * i
+    for i in range(N // 2 + 1, N + 1) :
+        F += i
 print(F)
