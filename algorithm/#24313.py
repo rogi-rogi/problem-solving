648a619fc0fcb10f052e9acab5105f008de34c0d
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-10-12 23:13:16 +0900

    Update #24313.py

--- algorithm/#24313.py
+++ algorithm/#24313.py
@@ -1,5 +1,17 @@
+def O() :
+    for n in range(n0, 101) :
+        if (a1 - c) * n + a0 > 0 : return 0
+    return 1
+    
+a1, a0 = map(int, input().split())
+c = int(input())
+n0 = int(input())
+print(O())
+
+'''
 a1, a0 = map(int, input().split())
 c = int(input())
 n0 = int(input())
 print([1, 0][(a1 > c) or (a1 == c and a0 > 0) or (a1 * n0 + a0 > c * n0)])
 # (c - a1) * n - a0 >= 0, c - a1 >= 0, n0을 만족하면 n0 <= 도 만족한다.
+'''
