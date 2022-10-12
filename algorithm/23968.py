15b20e5da4392850fbdc6393e432967859c1c008
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-10-12 18:55:36 +0900

    Update 23968.py

--- algorithm/23968.py
+++ algorithm/23968.py
@@ -1,13 +1,16 @@
 #pypy3
+def bubbleSort() :
+    swap_cnt = 0
+    for i in range(N, 0, -1) :
+        for j in range(i - 1) :
+            if arr[j] > arr[j + 1] :
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swap_cnt += 1
+                if swap_cnt == K : 
+                    print(arr[j], arr[j + 1])
+                    return 0
+    return -1
+    
 N, K = map(int, input().split())
-arr = list(map(int, input().split()))
-change = 0
-for i in range(N, 0, -1) :
-    for j in range(i - 1) :
-        if arr[j] > arr[j + 1] :
-            change += 1
-            arr[j], arr[j + 1] = arr[j + 1], arr[j]
-            if change == K :
-                print(arr[j], arr[j + 1])
-                exit(0)
-print(-1)
+arr = [*map(int, input().split())]
+if bubbleSort() == -1 : print(-1)
