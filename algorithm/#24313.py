734d83ae9110b19ab32752169bdba4aaabdced83
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-10-12 23:05:39 +0900

    Update #24313.py

--- algorithm/#24313.py
+++ algorithm/#24313.py
@@ -1,4 +1,5 @@
 a1, a0 = map(int, input().split())
 c = int(input())
 n0 = int(input())
-print([1, 0][(a1 > c) or (a1 == c and a0 > 0) or (a1 * n + a0 > c * n)])
+print([1, 0][(a1 > c) or (a1 == c and a0 > 0) or (a1 * n0 + a0 > c * n0)])
+# (c - a1) * n - a0 >= 0, c - a1 >= 0, n0을 만족하면 n0 <= 도 만족한다.
