c3379f8bdcd4e9c772b069be75741655c5aff4cf
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-10-12 20:12:19 +0900

    Update 24053.py

--- algorithm/24053.py
+++ algorithm/24053.py
@@ -1,20 +1,18 @@
 #pypy3
-N = int(input())
-A = list(map(int, input().split()))
-B = list(map(int, input().split()))
-if A == B : print(1)
-else :
+def insertionSort() :
+    if A == B : return 1
     for i in range(1, N) :
         temp = A[i]
         if A[i - 1] > temp :
             while A[i - 1] > temp and i >= 1 :
                 A[i] = A[i - 1]
-                if A[i - 1] == B[i - 1] and B[i - 1] == B[i] and A == B :
-                    print(1)
-                    exit(0)
+                if A[i - 1] == B[i - 1] == B[i] and A == B : return 1
                 i -= 1
             A[i] = temp
-            if A == B: 
-                print(1)
-                exit(0)
-    print(0)
+            if A[i] == B[i] and A == B : return 1
+    return 0
+
+N = int(input())
+A = [*map(int, input().split())]
+B = [*map(int, input().split())]
+print(insertionSort())
