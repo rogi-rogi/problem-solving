8cbe4201d408cec49ba2b8c2b58d659b0fd67477
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-10-12 18:59:20 +0900

    Update 23969.py

--- algorithm/23969.py
+++ algorithm/23969.py
@@ -3,13 +3,13 @@ def bubbleSort() :
     swap_cnt = 0
     for i in range(N, 0, -1) :
         for j in range(i - 1) :
-            if arr[j] > arr[j + 1] :
+            if A[j] > A[j + 1] :
                 swap_cnt += 1
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                A[j], A[j + 1] = A[j + 1], A[j]
                 if swap_cnt == K : return swap_cnt
     return swap_cnt
     
 N, K = map(int, input().split())
-arr = [*map(int, input().split())]
-if bubbleSort() == K : print(*arr)
+A = [*map(int, input().split())]
+if bubbleSort() == K : print(*A)
 else : print(-1)
