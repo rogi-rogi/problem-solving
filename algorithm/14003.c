1d0ce63caba6ad836fa1cca5401111f1dac8bb71
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-10-14 22:50:20 +0900

    Update 14003.c

--- algorithm/14003.c
+++ algorithm/14003.c
@@ -1,46 +1,40 @@
-#include <stdio.h>
+#include <algorithm>
+#include <iostream>
+#include <vector>
 #define SIZE 1000000
+using namespace std;
 
-int A[SIZE], dp[SIZE], index_list[SIZE];
+vector<int> A(SIZE), index_list(SIZE), dp;
 
 void print_LIS(int length, int idx)
 {
     if (idx == -1) return;
     if (index_list[idx] == length) {
         print_LIS(length - 1, idx - 1);
-        printf("%d ", A[idx]);
+        cout << A[idx] << ' ';
     } else print_LIS(length, idx - 1);
-}
-
-int lowerBound(int *arr, int right, int key)
-{
-    int left = 0, mid;
-    while (left < right) {
-        if (arr[mid = (left + right) / 2] < key) left = mid + 1;
-        else right = mid;
-    }
-    return left;
 }
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
     int N;
-    scanf("%d", &N);
-    scanf("%d", &A[0]);
-    dp[0] = A[0];
-    index_list[0] = 0;
-    int idx = 1, temp_idx;
-    for(int i = 1; i < N; ++i) {
-        scanf("%d", &A[i]);
-        if (A[i] > dp[idx - 1]) {
-            dp[idx] = A[i];
-            index_list[i] = idx++;
+    cin >> N >> A[0];
+    dp.push_back(A[0]);
+    index_list.push_back(0);
+    int LIS_length = 1;
+    for (int i = 1; i < N; ++i) {
+        cin >> A[i];
+        if (A[i] > dp.back()) {
+            dp.push_back(A[i]);
+            index_list[i] = LIS_length++;
         } else {
-            temp_idx = lowerBound(dp, idx - 1, A[i]);
-            dp[temp_idx] = A[i];
-            index_list[i] = temp_idx;
+            int idx = lower_bound(dp.begin(), dp.end(), A[i]) - dp.begin();
+            dp[idx] = A[i];
+            index_list[i] = idx;
         }
     }
-    printf("%d\n", idx);
-    print_LIS(idx - 1, N - 1);
+    cout << LIS_length << '\n';
+    print_LIS(LIS_length - 1, N - 1);
 }
