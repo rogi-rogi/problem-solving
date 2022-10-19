56633ebf7a9b5c148cf7457e99f8402314b1ff9e
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-10-19 22:57:24 +0900

    Update #11052.cpp

--- algorithm/#11052.cpp
+++ algorithm/#11052.cpp
@@ -7,7 +7,7 @@ int main()
     int N;
     cin >> N;
     vector<int> P(N + 1), dp(N + 1);
-    for (int i = 1;i <= N; ++i) cin >> P[i];
+    for (int i = 1; i <= N; ++i) cin >> P[i];
     for (int i = 1; i <= N; ++i)
         for (int j = 1; j <= i; ++j)
             dp[i] = max(dp[i], dp[i - j] + P[j]);
