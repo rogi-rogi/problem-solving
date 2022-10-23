939e13736d30f219442b28b43d7c13e76b132828
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-10-23 22:15:40 +0900

    Create Dijkstra.py

---
+++ Algorithm/Dijkstra.py
@@ -0,0 +1,28 @@
+from heapq import heappop, heappush
+from math import inf
+from sys import stdin
+input = stdin.readline
+
+def dijkstra(v) : # start V 
+    pq = []
+    heappush(pq, (0, v))
+    dist[v] = 0
+    while pq :
+        w, v = heappop(pq)
+        if dist[v] < w : continue
+        for nv, nw in graph[v] :
+            nw = w + nw
+            if nw < dist[nv] :
+                dist[nv] = nw
+                heappush(pq, (nw, nv))
+                
+if __name__ == "__main__" :
+    V, E = map(int, input().split())
+    graph = [[] for _ in range(V + 1)]
+    dist = [inf] * (V + 1)
+    for _ in range(E) :
+        v1, v2, w = map(int, input().split())  # v1 --(w)--> v2
+        graph[v1].append((v2, w))
+    dijkstra(1)
+    for i in range(1, V + 1) :
+        print([dist[i], "INF"][dist[i] == inf])
