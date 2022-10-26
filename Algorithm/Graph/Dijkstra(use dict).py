9a60827b8c2734b6c8cb78482d46926217fda6b0
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-10-26 22:44:15 +0900

    Create Dijkstra(use dict).py

---
+++ Algorithm/Graph/Dijkstra(use dict).py
@@ -0,0 +1,28 @@
+from heapq import heappop, heappush
+from math import inf
+from sys import stdin
+input = stdin.readline
+
+def Dijkstra(v) : # start Vertex
+    pq = []
+    heappush(pq, (0, v))
+    dist[v] = 0
+    while pq :
+        w, v = heappop(pq)
+        if dist[v] < w : continue # distance previously calculated is shorter
+        for nv, nw in graph[v].items() :
+            nw += w
+            if nw < dist[nv] :
+                dist[nv] = nw
+                heappush(pq, (nw, nv))
+                
+if __name__ == "__main__" :
+    V, E = map(int, input().split())
+    graph = [dict() for _ in range( + 1)]
+    dist = [inf] * (V + 1)
+    for _ in range(E) :
+        v1, v2, w = map(int, input().split())  # v1 --(w)--> v2 
+        graph[v1][v2] = min(graph[v1][v2], w) if v2 in graph[v1].keys() else w
+    Dijkstra(1)
+    for i in range(1, V + 1) :
+        print([dist[i], "INF"][dist[i] == inf])
