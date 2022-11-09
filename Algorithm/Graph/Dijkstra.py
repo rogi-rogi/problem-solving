59bcfebf4d7666ce6319b3b2346b931cf690c3d1
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-11-09 23:05:47 +0900

    Update Dijkstra.py

--- Algorithm/Graph/Dijkstra.py
+++ Algorithm/Graph/Dijkstra.py
@@ -21,7 +21,7 @@ def Dijkstra(v) : # start Vertex
     while pq :
         w, v = heappop(pq)
         if dist[v] < w : continue # distance previously calculated is shorter
-        for nv, nw in graph[v] :
+        for nv, nw in graph[v].items() : # graph[v] :
             nw += w
             if nw < dist[nv] :
                 dist[nv] = nw
@@ -29,10 +29,11 @@ def Dijkstra(v) : # start Vertex
                 
 if __name__ == "__main__" :
     V, E = map(int, input().split())
-    graph = [[] for _ in range(V + 1)]
+    # graph = [[] for _ in range(V + 1)]  # The edge may not be given for both vertex.
+    graph = [dict() for _ in range(V + 1)]
     dist = [inf] * (V + 1)
     for _ in range(E) :
-        v1, v2, w = map(int, input().split())  # v1 --(w)--> v2 
-        graph[v1].append((v2, w))
+        v1, v2, w = map(int, input().split())  # v1 --(w)--> v2
+        graph[v1][v2] = min(graph[v1][v2], w) if v2 in graph[v1].keys() else w # graph[v1].append((v2, w))
     Dijkstra(1)
     print(*dist[1:])
