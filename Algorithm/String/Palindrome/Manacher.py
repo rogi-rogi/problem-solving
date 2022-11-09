9066f707dda945dcc9bf6255d206218d374096b8
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-11-09 19:25:57 +0900

    Update Manacher.py

--- Algorithm/String/Palindrome/Manacher.py
+++ Algorithm/String/Palindrome/Manacher.py
@@ -7,9 +7,11 @@ def Manacher(s) :
     for i in range(SIZE) :
         # if r < i : R[i] = 0 # away from parent palindrome
         if i <= r : R[i] = min(r - i, R[c + c - i])
+        # min(palindrome on the other side, guaranteed that 's[i]'' is palindrome until leaves parent palindrome)
+        
         while (0 <= i - R[i] - 1 and i + R[i] + 1 < SIZE) and s[i - R[i] - 1] == s[i + R[i] + 1] :
             R[i] += 1
-        if r < i + R[i] : # away from parent palindrome or find other big palindrome
+        if r < i + R[i] :
             r = i + R[i]
             c = i
     return max(R)
