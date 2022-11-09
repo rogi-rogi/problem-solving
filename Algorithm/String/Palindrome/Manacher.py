b6e01307e6d3f9ef1b6941f2e725fc2fa71e6e29
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-11-09 16:35:38 +0900

    Create Manacher.py

---
+++ Algorithm/String/Palindrome/Manacher.py
@@ -0,0 +1,18 @@
+def Manacher(s) :
+    s = '#' + '#'.join(s) + '#'
+    SIZE = len(s)
+    r = 0 # 'r' is last index of sub-palindrome
+    c = 0 # 'c' is center index of sub-palindrome
+    R = [0] * SIZE # 'R[i]' is radius of sub-palindrome centered on 's[i]'
+    for i in range(SIZE) :
+        # if r < i : R[i] = 0 # away from parent palindrome
+        if i <= r : R[i] = min(r - i, R[c + c - i])
+        while (0 <= i - R[i] - 1 and i + R[i] + 1 < SIZE) and s[i - R[i] - 1] == s[i + R[i] + 1] :
+            R[i] += 1
+        if r < i + R[i] : # away from parent palindrome or find other big palindrome
+            r = i + R[i]
+            c = i
+    return max(R)
+
+if __name__ == "__main__" :
+    print(Manacher(input()))
