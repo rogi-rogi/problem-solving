d077b3ef7d55989841b29834354b0a6ea3643cc4
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-11-11 08:29:04 +0900

    Update and rename #05972.py to 05972.py

--- unsolved/05972.py
+++ unsolved/05972.py
@@ -4,27 +4,27 @@ from sys import stdin
 input = stdin.readline
 
 def Dijkstra(v) :
+    dist = [inf] * (N + 1)
+    dist[v] = 0
     pq =[]
     heappush(pq, (0, v))
-    dist[v] = 0
     while pq :
         w, v = heappop(pq)
         if dist[v] < w : continue
-        if v == N : return
+        if v == N : return dist
         for nv, nw in graph[v] :
             nw += w
             if nw < dist[nv] :
                 dist[nv] = nw
                 heappush(pq, (nw, nv))
+    return dist
 
 if __name__ == "__main__" :
     N, M = map(int, input().split())
     graph = [[] for _ in range(N + 1)]
-    dist = [inf] * (N + 1)
     for _ in range(M) :
         A_i, B_i, C_i = map(int, input().split())
         graph[A_i].append((B_i, C_i))
         graph[B_i].append((A_i, C_i))
-    Dijkstra(1)
+    dist = Dijkstra(1)
     print(dist[N])
-        
