55229ba5a2fc7a6d0764a7988310ff1f0732076a
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-11-10 23:15:05 +0900

    Update BellmanFord.py

--- Algorithm/Graph/BellmanFord.py
+++ Algorithm/Graph/BellmanFord.py
@@ -2,14 +2,18 @@ from math import inf
 from sys import stdin
 input = stdin.readline
 
+'''
+[ Time Complexity ] : O (VE)
+target : graph with negative cycle
+'''
+
 def BellmanFord(v) :
     dist = [inf] * (V + 1)
     dist[v] = 0
-    for i in range(V) :
-        for j in range(E) :
-            v1, v2, w = edges[j]
+    for v in range(V) :
+        for v1, v2, w in edges :
             if dist[v1] != inf and dist[v1] + w < dist[v2] :
-                if i == V - 1 : return False
+                if v == V - 1 : return False    # find negative cycle.
                 dist[v2] = dist[v1] + w
     return dist
 
