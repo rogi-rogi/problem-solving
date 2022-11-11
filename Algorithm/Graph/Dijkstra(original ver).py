2df8e2a2d0935dc582897edc678703309144249a
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-11-11 23:42:23 +0900

    Update Dijkstra(original ver).py

--- Algorithm/Graph/Dijkstra(original ver).py
+++ Algorithm/Graph/Dijkstra(original ver).py
@@ -8,7 +8,7 @@ def Dijkstra(v) :
     dist[v] = 0
     visited = [False] * (V + 1)
     visited[v] = True
-    for av, aw in graph[v] : # vertex adjacent to the start vertex
+    for av, aw in edges[v] : # vertex adjacent to the start vertex
         dist[av] = aw
     for _ in range(V - 1) :  # other vertex
         min_weighted = inf
@@ -18,7 +18,7 @@ def Dijkstra(v) :
                 min_weighted = dist[av]
                 nv = av
         visited[nv] = True
-        for av, aw in graph[nv] :
+        for av, aw in edges[nv] :
             nw = dist[nv] + aw
             if nw < dist[av] :
                 dist[av] = nw
@@ -26,9 +26,9 @@ def Dijkstra(v) :
                 
 if __name__ == "__main__" :
     V, E = map(int, input().split())
-    graph = [[] for _ in range(V + 1)]
+    edges = [[] for _ in range(V + 1)]
     for _ in range(E) :
         v1, v2, w = map(int, input().split())
-        graph[v1].append((v2, w))
+        edges[v1].append((v2, w))
     dist = Dijkstra(1)
     print(*dist[1:])
