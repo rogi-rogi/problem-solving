7cd99cf9950024bcfda51e34d2c89725ee422d29
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-11-12 10:08:39 +0900

    Update #19638.py

--- unsolved/#19638.py
+++ unsolved/#19638.py
@@ -1,25 +1,22 @@
-import heapq
+from heapq import heappop, heappush
 from sys import stdin
 input = stdin.readline
 
 N, H_centi, T = map(int, input().split())
 heap = []
 cnt = 0
-for _ in range(N) :
-    heapq.heappush(heap, -int(input()))
+for _ in range(N) : 
+    heappush(heap, -int(input()))
 for _ in range(T) :
-    temp = -heapq.heappop(heap)
+    temp = -heappop(heap)
     if temp < H_centi :
-        heapq.heappush(heap, temp)
+        heappush(heap, temp)
         break
     elif temp != 1 :
         cnt += 1
-        heapq.heappush(heap, - (temp // 2))
-    else : heapq.heappush(heap, -1)
-temp = -heapq.heappop(heap)
-if temp < H_centi :
-    print("YES")
-    print(cnt)
-else :
-    print("NO")
-    print(temp)
+        heappush(heap, - (temp // 2))
+    else : heappush(heap, -1)
+    
+temp = -heappop(heap)
+if temp < H_centi : print("YES", cnt, sep = '\n')
+else : print("NO", temp, sep = '\n')
