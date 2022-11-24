5d2e48487be880bba0710072b8829cea9b86422f
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-11-24 23:30:29 +0900

    Update 14284.py

--- unsolved/14284.py
+++ unsolved/14284.py
@@ -6,13 +6,12 @@ input = stdin.readline
 def Dijkstra(v) :
     dist = [inf] * (n + 1)
     dist[v] = 0
-    pq = []
-    heappush(pq, (0, v))
+    pq = [(0, v)]
     while pq :
         w, v = heappop(pq)
         if v == t : return dist
         if dist[v] < w : continue
-        for nv, nw in edges[v] :
+        for nv, nw in edges[v].items() :
             nw += w
             if nw < dist[nv] :
                 dist[nv] = nw
@@ -24,8 +23,8 @@ if __name__ == "__main__" :
     edges = [[] for _ in range(n + 1)]
     for _ in range(m) :
         a, b, c = map(int, input().split())
-        edges[a].append((b, c))
-        edges[b].append((a, c))
+        edges[a][b] = min(edges[a][b], c) if b in edges[a].keys() else c
+        edges[b][a] = min(edges[b][a], c) if a in edges[b].keys() else c
     s, t = map(int, input().split())
     dist = Dijkstra(s)
     print(dist[t])
