219a390e839b7d09ed1daca91e36b4684f992e93
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-11-24 23:29:03 +0900

    Update 01916.py

--- solved/01916.py
+++ solved/01916.py
@@ -6,25 +6,25 @@ input = stdin.readline
 def Dijkstra(v) :
     dist = [inf] * (N + 1)
     dist[v] = 0
-    pq = []
-    heappush(pq, (0, v))
+    pq = [(0, v)]
     while pq :
         w, v = heappop(pq)
         if dist[v] < w : continue
         if v == v2 : return dist
-        for nv, nw in graph[v] :
+        for nv, nw in edges[v].items() :
             nw += w
             if nw < dist[nv] :
                 dist[nv] = nw
                 heappush(pq, (nw, nv))
     return dist
+    
 if __name__ == "__main__" :
     N = int(input())
     M = int(input())
-    graph = [[] for _ in range(N + 1)]
+    edges = [dict() for _ in range(N + 1)]
     for _ in range(M) :
         v1, v2, w = map(int, input().split())
-        graph[v1].append((v2, w))
+        edges[v1][v2] = min(edges[v1][v2], w) if v2 in edges[v1].keys() else w
     v1, v2 = map(int, input().split())
     dist = Dijkstra(v1)
     print(dist[v2])
