81c3c86d4789976394f3e441bbc4a783898568fe
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-11-29 23:49:34 +0900

    Create 01197.py

---
+++ unsolved/01197.py
@@ -0,0 +1,27 @@
+from heapq import heappop, heappush
+from sys import stdin
+input = stdin.readline
+
+def Prim(v) :
+    visited = [False] * (V + 1)
+    visited[v] = True
+    pq = []
+    for v2, w in edges[v].items() : heappush(pq, (w, v, v2))
+    MST_weight = 0
+    while pq :
+        w, v1, v2 = heappop(pq)
+        if not visited[v2] :
+            visited[v2] = True
+            MST_weight += w
+            for nv, nw in edges[v2].items() :
+                if not visited[nv] : heappush(pq, (nw, v2, nv))
+    return MST_weight
+  
+if __name__ == "__main__" :
+    V, E = map(int, input().split())
+    edges = [dict() for _ in range(V + 1)]
+    for _ in range(E) :
+        v1, v2, w = map(int, input().split())
+        edges[v1][v2] = min(edges[v1][v2], w) if v2 in edges[v1].keys() else w
+        edges[v2][v1] = min(edges[v2][v1], w) if v1 in edges[v2].keys() else w
+    print(Prim(1))
