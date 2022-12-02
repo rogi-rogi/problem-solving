74c2676aa57d9d7571d32282f0693387929d01c6
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-12-02 16:32:55 +0900

    Update 01647.py

--- unsolved/01647.py
+++ unsolved/01647.py
@@ -1,37 +1,39 @@
-from sys import setrecursionlimit, stdin
-setrecursionlimit(10 ** 8)
+from sys import stdin
 input = stdin.readline
 
-def Kruskal() :
-    def find(v) :
-        if parent[v] == v : return v
-        parent[v] = find(parent[v])
-        return parent[v]
-
-    def union(v1, v2) :
-        v1 = find(v1)
-        v2 = find(v2)
-        if v1 != v2 : 
-            parent[max(v1, v2)] = min(v1, v2)
+class DisjointSet :
+    def __init__(self, V) :
+        self.parents = [*range(V + 1)]
+    
+    def find(self, v) :
+        if self.parents[v] == v : return v
+        self.parents[v] = self.find(self.parents[v])
+        return self.parents[v]
+        
+    def union(self, v1, v2) :
+        v1 = self.find(v1)
+        v2 = self.find(v2)
+        if v1 != v2 :
+            self.parents[max(v1, v2)] = min(v1, v2)
             return True
         return False
-    
-    parent = [i for i in range(V + 1)]
+
+def Kruskal(V, divide = 1) :
+    graph = DisjointSet(V)
     MST_weight = 0
-    edge_cnt = 0
+    edges_cnt = 0
     for w, v1, v2 in edges :
-        if union(v1, v2) : 
-            if edge_cnt >= V - 2 : break
-            edge_cnt += 1
+        if graph.union(v1, v2) :
+            edges_cnt += 1
             MST_weight += w
+            if edges_cnt >= V - divide : break
     return MST_weight
     
 if __name__ == "__main__" :
     V, E = map(int, input().split())
     edges = []
-    MST_weight = 0
     for _ in range(E) :
         v1, v2, w = map(int, input().split())
         edges.append((w, v1, v2))
     edges.sort()
-    print(Kruskal())
+    print(Kruskal(V, 2))
