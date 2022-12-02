3954de0286f00e1623e8570186f9bf2f5fdb9c2b
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-12-03 00:16:04 +0900

    Update Prim.py

--- Algorithm/Graph/MST/Prim.py
+++ Algorithm/Graph/MST/Prim.py
@@ -13,7 +13,6 @@ def Prim(v, divide = 1) :
     visited[v] = True
     pq = [(w, v, v2) for v2, w in edges[v].items()]
     heapify(pq)
-    MST = []
     MST_weights = []
     connected_edge = 0
     while pq :
@@ -21,12 +20,11 @@ def Prim(v, divide = 1) :
         if not visited[v2] :
             visited[v2] = True
             connected_edge += 1
-            MST.append((v1, v2))
             MST_weights.append(w)
             for nv, nw in edges[v2].items() :
                 if not visited[nv] : heappush(pq, (nw, v2, nv))
         if connected_edge >= V - 1 : break
-    return MST, sum(MST_weights) - sum(sorted(MST_weights, reverse = True)[:divide - 1])
+    return sum(MST_weights) - sum(sorted(MST_weights, reverse = True)[:divide - 1])
   
 if __name__ == "__main__" :
     V, E = map(int, input().split())
@@ -35,6 +33,4 @@ if __name__ == "__main__" :
         v1, v2, w = map(int, input().split())
         edges[v1][v2] = min(edges[v1][v2], w) if v2 in edges[v1].keys() else w
         edges[v2][v1] = min(edges[v2][v1], w) if v1 in edges[v2].keys() else w
-    MST, MST_weight = Prim(1)
-    print(*MST)
-    print(MST_weight)
+    print(Prim(1))
