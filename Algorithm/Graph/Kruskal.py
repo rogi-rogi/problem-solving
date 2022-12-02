d40bee1099176180c2937702e933f5d964894d9f
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-12-02 14:06:08 +0900

    Create Kruskal.py

---
+++ Algorithm/Graph/Kruskal.py
@@ -0,0 +1,38 @@
+'''
+
+'''
+class DisjointSet :
+    def __init__(self, V) :
+        self.parents = [*range(V + 1)]
+    
+    def find(self, v) :
+        if self.parents[v] == v : return v
+        self.parents[v] = self.find(self.parents[v])
+        return self.parents[v]
+        
+    def union(self, v1, v2) :
+        v1 = self.find(v1)
+        v2 = self.find(v2)
+        if v1 != v2 :
+            self.parents[max(v1, v2)] = min(v1, v2)
+            return True
+        return False
+
+def Kruskal(V) :
+    graph = DisjointSet(V)
+    MST_weight = 0
+    edges_cnt = 0
+    for w, v1, v2 in edges :
+        edges_cnt += 1
+        if graph.union(v1, v2) :
+            MST_weight += w
+    return MST_weight
+    
+if __name__ == "__main__" :
+    V, E = map(int, input().split())
+    edges = []
+    for _ in range(E) :
+        v1, v2, w = map(int, input().split())
+        edges.append((w, v1, v2))
+    edges.sort()
+    print(Kruskal(V))
