a85e730d65b2b0aa2983ca60c39218fe0c180d34
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-12-03 22:15:52 +0900

    Update 01922.py

--- unsolved/01922.py
+++ unsolved/01922.py
@@ -4,37 +4,33 @@ input = stdin.readline
 class DisjointSet :
     def __init__(self, V) :
         self.parents = [*range(V + 1)]
-    
+        self.weight = 0
+        self.connected_edge = 0
+        
     def find(self, v) :
         if self.parents[v] == v : return v
         self.parents[v] = self.find(self.parents[v])
         return self.parents[v]
         
-    def union(self, v1, v2) :
+    def union(self, v1, v2, w) :
         v1 = self.find(v1)
         v2 = self.find(v2)
         if v1 != v2 :
             self.parents[max(v1, v2)] = min(v1, v2)
+            self.weight += w
+            self.connected_edge += 1
             return True
         return False
-
+        
 def Kruskal(V) :
     graph = DisjointSet(V)
-    MST_weight = 0
-    edges_cnt = 0
-    for w, v1, v2 in edges :
-        if graph.union(v1, v2) :
-            edges_cnt += 1
-            MST_weight += w
-            if edges_cnt >= V - 1 : break
-    return MST_weight
+    for v1, v2, w in edges :
+        if graph.union(v1, v2, w) :
+            if graph.connected_edge >= V - 1 : break
+    return graph.weight
     
 if __name__ == "__main__" :
     V = int(input())
     E = int(input())
-    edges = []
-    for _ in range(E) :
-        v1, v2, w = map(int, input().split())
-        edges.append((w, v1, v2))
-    edges.sort()
+    edges = sorted([tuple(map(int, input().split())) for _ in range(E)], key = lambda x : x[2])
     print(Kruskal(V))
