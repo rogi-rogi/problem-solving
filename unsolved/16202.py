1bb307a5b46fb4096cd57b03d54d366e2d212769
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-12-07 12:22:04 +0900

    Update 16202.py

--- unsolved/16202.py
+++ unsolved/16202.py
@@ -2,31 +2,29 @@ from sys import stdin
 input = stdin.readline
 
 class DisjointSet :
-    def __init__(self, V) :
-        self.parents = [*range(V + 1)]
-        self.weight = 0
-        self.connected_edge = 0
+    def find(self, v, parents) :
+        if parents[v] == v : return v
+        parents[v] = self.find(parents[v], parents)
+        return parents[v]
         
-    def find(self, v) :
-        if self.parents[v] == v : return v
-        self.parents[v] = self.find(self.parents[v])
-        return self.parents[v]
-        
-    def union(self, v1, v2, w) :
-        v1 = self.find(v1)
-        v2 = self.find(v2)
+    def union(self, v1, v2, parents) :
+        v1 = self.find(v1, parents)
+        v2 = self.find(v2, parents)
         if v1 != v2 :
-            self.parents[max(v1, v2)] = min(v1, v2)
-            self.weight += w
-            self.connected_edge += 1
+            parents[max(v1, v2)] = min(v1, v2)
             return True
         return False
 
-def Kruskal(V, k) :
-    graph = DisjointSet(V)
+def Kruskal(V) :
+    parents = [*range(V + 1)]
+    MST_weight = 0
+    connected_edge = 0
+    graph = DisjointSet()
     for w, v1, v2 in edges :
-        if graph.union(v1, v2, w) :
-            if graph.connected_edge >= V - 1 : return graph.weight
+        if graph.union(v1, v2, parents) :
+            MST_weight += w
+            connected_edge += 1
+            if connected_edge >= V - 1 : return MST_weight
     return -1
     
 if __name__ == "__main__" :
@@ -37,8 +35,8 @@ if __name__ == "__main__" :
         edges.append((e, v1, v2))
     edges.sort()
     res = []
-    for k in range(K) :
-        weight = Kruskal(V, k)
+    for _ in range(K) :
+        weight = Kruskal(V)
         res.append((weight, 0)[weight == -1])
         del edges[0]
     print(*res)
