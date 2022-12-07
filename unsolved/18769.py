ea2cc671f943a43df3543a614d1076e55ac10b2e
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-12-07 17:10:42 +0900

    Update 18769.py

--- unsolved/18769.py
+++ unsolved/18769.py
@@ -3,48 +3,46 @@ input = stdin.readline
 
 '''
 class DisjointSet :
-    def __init__(self, V) :
-        self.parents = [*range(V + 1)]
-        self.weight = 0
-        self.connected_edge = 0
-        
-    def find(self, v) :
-        if self.parents[v] == v : return v
-        self.parents[v] = self.find(self.parents[v])
-        return self.parents[v]
-        
-    def union(self, v1, v2, w) :
-        v1 = self.find(v1)
-        v2 = self.find(v2)
-        if v1 != v2 :
-            self.parents[max(v1, v2)] = min(v1, v2)
-            self.weight += w
-            self.connected_edge += 1
-            return True
-        return False
-
-def Kruskal(V) :
-    graph = DisjointSet(V)
-    for w, v1, v2 in edges :
-        if graph.union(v1, v2, w) :
-            if graph.connected_edge >= V - 1 : break
-    return graph.weight
-''' # slow...
-
-def Kruskal(V) :
-    def find(parents, v) :
+    def find(self, v, parents) :
         if parents[v] == v : return v
-        parents[v] = find(parents, parents[v])
+        parents[v] = self.find(parents[v], parents)
         return parents[v]
-    
-    def union(parents, v1, v2) :
-        v1 = find(parents, v1)
-        v2 = find(parents, v2)
+        
+    def union(self, v1, v2, parents) :
+        v1 = self.find(v1, parents)
+        v2 = self.find(v2, parents)
         if v1 != v2 :
             parents[max(v1, v2)] = min(v1, v2)
             return True
         return False
+
+def Kruskal(V, divide = 1) :
+    parents = [*range(V + 1)]
+    MST_weight = 0
+    connected_edge = 0
+    graph = DisjointSet()
+    for w, v1, v2 in edges :
+        if graph.union(v1, v2, parents) :
+            MST_weight += w
+            connected_edge += 1
+            if connected_edge >= V - divide : break
+    return MST_weight
+''' # slow...
+
+def find(parents, v) :
+    if parents[v] == v : return v
+    parents[v] = find(parents, parents[v])
+    return parents[v]
     
+def union(parents, v1, v2) :
+    v1 = find(parents, v1)
+    v2 = find(parents, v2)
+    if v1 != v2 :
+        parents[max(v1, v2)] = min(v1, v2)
+        return True
+    return False
+
+def Kruskal(V) :
     parents = [*range(V + 1)]
     MST_weight = 0
     connected_edge = 0
