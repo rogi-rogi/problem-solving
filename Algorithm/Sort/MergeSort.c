1ccc7dc73614e821cb35f4f08f43fe9b85a6dc8a
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-12-23 22:39:26 +0900

    Update MergeSort.c

--- Algorithm/Sort/MergeSort.c
+++ Algorithm/Sort/MergeSort.c
@@ -8,12 +8,18 @@ static void __MergeSort(int left, int right)
     int mid = (left + right) / 2;
     __MergeSort(left, mid);
     __MergeSort(mid + 1, right);
+    /*
     int idx = left, buff_p = 0;
     while (idx <= mid) buff[buff_p++] = arr[idx++];
     int sort_p = left, buff_idx = 0;
+    */
+    memcpy(buff, &arr[left], sizeof(int) * (mid - left + 1));
+    int idx = mid + 1, sort_p = left;
+    int buff_idx = 0, buff_p = mid - left + 1; 
+   
     while (idx <= right && buff_idx < buff_p)
       arr[sort_p++] = ((buff[buff_idx] <= arr[idx]) ? buff[buff_idx++] : arr[idx++]);
-    while (buff_idx < buff_p) arr[sort_p++] = buff[buff_idx++];
+    memcpy(&arr[sort_p], &buff[buff_idx], sizeof(int) * (buff_p - buff_idx)); // while (buff_idx < buff_p) arr[sort_p++] = buff[buff_idx++];
   }
 }
 
