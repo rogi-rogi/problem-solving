c9b3180034cf31a2078f7c2871c524212be8ce3a
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-12-23 23:07:18 +0900

    Update Inversion Counting(Merge Sort).cpp

--- Algorithm/Inversion Counting/Inversion Counting(Merge Sort).cpp
+++ Algorithm/Inversion Counting/Inversion Counting(Merge Sort).cpp
@@ -10,39 +10,39 @@ long long res = 0;
 
 static void __mergeSort(int left, int right)
 {
-  if (left < right) {
-    int mid = (left + right) / 2;
-    __mergeSort(left, mid);
-    __mergeSort(mid + 1, right);
-    /*
-    int idx = left, buff_p = 0;
-    while (idx <= mid) buff[buff_p++] = arr[idx++];
-    int sort_p = left, buff_idx = 0;
-    */
-    
-    copy(&arr[left], &arr[mid + 1], buff);
-    int idx = mid + 1, sort_p = left;
-    int buff_idx = 0, buff_p = mid - left + 1; 
-    
-    while (buff_idx < buff_p && idx <= right)
-      if (buff[buff_idx] <= arr[idx]) arr[sort_p++] = buff[buff_idx++];
-      else {
-        arr[sort_p++] = arr[idx++];
-        res += (long long)(buff_p - buff_idx);
-      }
-    copy(&buff[buff_idx], &buff[buff_p], &arr[sort_p]); // while (buff_idx < buff_p) arr[sort_p++] = buff[buff_idx++];
-  }
+    if (left < right) {
+        int mid = (left + right) / 2;
+        __mergeSort(left, mid);
+        __mergeSort(mid + 1, right);
+        /*
+        int idx = left, buff_p = 0;
+        while (idx <= mid) buff[buff_p++] = arr[idx++];
+        int sort_p = left, buff_idx = 0;
+        */
+
+        copy(&arr[left], &arr[mid + 1], buff);
+        int idx = mid + 1, sort_p = left;
+        int buff_idx = 0, buff_p = mid - left + 1; 
+
+        while (buff_idx < buff_p && idx <= right)
+            if (buff[buff_idx] <= arr[idx]) arr[sort_p++] = buff[buff_idx++];
+            else {
+                arr[sort_p++] = arr[idx++];
+                res += (long long)(buff_p - buff_idx);
+            }
+        copy(&buff[buff_idx], &buff[buff_p], &arr[sort_p]); // while (buff_idx < buff_p) arr[sort_p++] = buff[buff_idx++];
+    }
 }
 
 void mergeSort(int size) { __mergeSort(0, size - 1); }
 
 int main()
 {
-  ios_base::sync_with_stdio(false);
-  cin.tie(nullptr);
-  int N;
-  cin >> N;
-  for (int i = 0; i < N; ++i) cin >> arr[i];
-  mergeSort(N);
-  cout << res;
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int N;
+    cin >> N;
+    for (int i = 0; i < N; ++i) cin >> arr[i];
+    mergeSort(N);
+    cout << res;
 }
