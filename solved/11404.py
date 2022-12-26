2faf3015f55cc96ff85bbb7f881e25ef42a559aa
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-12-26 22:22:56 +0900

    Update 11404.py

--- solved/11404.py
+++ solved/11404.py
@@ -19,9 +19,5 @@ if __name__ == "__main__" :
         v1, v2, w = map(int, input().split())
         if edges[v1][v2] > w : edges[v1][v2] = w
     FloydWarshall()
-    del edges[0]
-    for edge in edges :
-        del edge[0]
-        for e in edge :
-            print([e, 0][e == inf], end = ' ')
-        print()
+    for v1 in range(1, V + 1) :
+        print(*[edges[v1][v2] for v2 in range(1, V + 1) if edges[v1][v2] != inf])
