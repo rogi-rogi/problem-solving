56276b28bdd6c90ef34004f6db87732303ce14db
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-12-28 19:04:00 +0900

    Create 09463.cpp

---
+++ unsolved/09463.cpp
@@ -0,0 +1,50 @@
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <vector>
+using namespace std;
+typedef long long ll;
+
+vector<int> tree;
+
+void update(int i) { for (; i < tree.size(); i += (i & -i)) ++tree[i]; }
+
+ll query(int i)
+{
+    ll res = 0;
+    for (; i; i -= (i & -i)) res += tree[i];
+    return res;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int T;
+    cin >> T;
+    while (T--) {
+        int N;
+        vector<ll> arr, edges;
+        {
+            cin >> N;
+            arr.resize(N + 1);
+            edges.resize(N + 1);
+            for (int i = 1, v1; i <= N; ++i) {
+                cin >> v1;
+                edges[v1] = i;
+            }
+            for (int i = 1, v2; i <= N; ++i) {
+                cin >> v2;
+                arr[i] = edges[v2];
+            }
+            tree.resize(N + 1);
+        }
+        ll res = 0;
+        for (int i = 1; i <= N; ++i) {
+            res += query(N) - query(arr[i]);
+            update(arr[i]);
+        }
+        cout << res << '\n';
+        vector<int>().swap(tree);
+    }
+}
