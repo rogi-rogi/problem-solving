856fb324379cd6ebd922c89a1fe00af8c2e3c002
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2023-01-04 16:08:51 +0900

    Update 16975.cpp

--- solved/16975.cpp
+++ solved/16975.cpp
@@ -5,37 +5,24 @@
 using namespace std;
 typedef long long ll;
 
-vector<ll> tree, leaf;
+vector<ll> lazy_tree, leaf;
 
-void init(int start, int end, int node = 1)
-{
-    if (start == end) tree[node] = leaf[start];
-    else {
-        int mid = (start + end)/2;
-        init(start, mid, node*2);
-        init(mid+1, end, node*2 +1);
-    }
-}
-
-void update(int start, int end, int i, int j, ll diff, int node = 1)
+void lazy_update(int start, int end, int i, int j, ll diff, int node = 1)
 {
     if (j < start || end < i) return;
-    if (i <= start && end <= j) tree[node] += diff;
+    if (i <= start && end <= j) lazy_tree[node] += diff;
     else {
         int mid = (start + end)/2;
-        update(start, mid, i, j, diff, node*2);
-        update(mid+1, end, i, j, diff, node*2 +1);
+        lazy_update(start, mid, i, j, diff, node*2);
+        lazy_update(mid+1, end, i, j, diff, node*2 +1);
     }
 }
 
 ll query(int start, int end, int idx, int node = 1)
 {
     if (end < idx || idx < start) return 0;
-    if (start == end) return tree[node];
-    int mid = (start + end)/2; 
-    ll left = query(start, mid, idx, node*2);
-    ll right = query(mid+1, end, idx, node*2 +1);
-    return left + right + (left ? tree[node] : 0) + (right ? tree[node] : 0);
+    if (start == end) return lazy_tree[node];
+    return (query(start, mid, idx, node*2) + query(mid+1, end, idx, node*2 +1) + lazy_tree[node]);
 }
 
 int main()
@@ -48,9 +35,8 @@ int main()
         leaf.resize(N + 1);
         for (int i = 1; i <= N; ++i) cin >> leaf[i];
         int size = (1 << ((int)ceil(log2(N)) + 1));
-        tree.resize(size);
+        lazy_tree.resize(size);
         cin >> M;
-        init(1, N);
     }
     int type;
     while (M--) {
@@ -59,12 +45,11 @@ int main()
             int i, j;
             ll k;
             cin >> i >> j >> k;
-            update(1, N, i, j, k);
+            lazy_update(1, N, i, j, k);
         } else {
             int x;
             cin >> x;
-            cout << query(1, N, x) << '\n';
+            cout << (leaf[x] + query(1, N, x)) << '\n';
         }
     }
 }
-
