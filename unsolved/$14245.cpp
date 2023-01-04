f12e4f502ed3f226ccc0794bc67c4e8df5dad9c6
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2023-01-04 16:05:49 +0900

    Create $14245.cpp

---
+++ unsolved/$14245.cpp
@@ -0,0 +1,58 @@
+#include <cmath>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+typedef long long ll;
+
+vector<ll> lazy_tree, leaf;
+
+void lazy_update(int start, int end, int i, int j, ll diff, int node = 1)
+{
+    if (j < start || end < i) return;
+    if (i <= start && end <= j) lazy_tree[node] ^= diff;
+    else {
+        int mid = (start + end)/2;
+        lazy_update(start, mid, i, j, diff, node*2);
+        lazy_update(mid+1, end, i, j, diff, node*2 +1);
+    }
+}
+
+ll query(int start, int end, int idx, int node = 1)
+{
+    if (end < idx || idx < start) return 0;
+    if (start == end) return lazy_tree[node];
+    int mid = (start + end)/2; 
+    ll left = query(start, mid, idx, node*2);
+    ll right = query(mid+1, end, idx, node*2 +1);
+    return left ^ right ^ lazy_tree[node];
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int N, M;
+    {
+        cin >> N;
+        leaf.resize(N + 1);
+        for (int i = 1; i <= N; ++i) cin >> leaf[i];
+        int size = (1 << ((int)ceil(log2(N)) + 1));
+        lazy_tree.resize(size);
+        cin >> M;
+    }
+    int type;
+    while (M--) {
+        cin >> type;
+        if (type == 1){
+            int i, j;
+            ll k;
+            cin >> i >> j >> k;
+            lazy_update(1, N, i+1, j+1, k);
+        } else {
+            int idx;
+            cin >> idx;
+            cout << (leaf[idx+1] ^ query(1, N, idx+1)) << '\n';
+        }
+    }
+}
