9b7eeb8864a09ceb47b82783e3f10a00fc2a08f4
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2023-01-16 22:32:14 +0900

    Update 01854.py

--- solved/01854.py
+++ solved/01854.py
@@ -10,11 +10,11 @@ def Dijkstra(v) :
     pq = [(0, v)]
     while pq :
         w, v = heappop(pq)
-        if dists[v][K - 1] < w : continue
+        if dists[v][-1] < w : continue
         for nv, nw in edges[v] :
             nw += w
-            if nw < dists[nv][K - 1] :
-                dists[nv][K - 1] = nw
+            if nw < dists[nv][-1] :
+                dists[nv][-1] = nw
                 dists[nv].sort()
                 heappush(pq, (nw, nv))
     return dists
@@ -27,4 +27,4 @@ if __name__ == "__main__" :
         edges[v1].append((v2, w))
     dists = Dijkstra(1)
     for i in range(1, V + 1) :
-        print(-1 if dists[i][K - 1] == inf else dists[i][K - 1])
+        print(-1 if dists[i][-1] == inf else dists[i][-1])
