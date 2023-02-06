9916fed9d26ea1b091e492c386f77e8b9d938376
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2023-02-06 09:45:47 +0900

    Update 04355.c

--- solved/04355.c
+++ solved/04355.c
@@ -6,20 +6,20 @@ ll euler_phi(ll n)
 {
     ll euler = n;
     for (ll prime = 2; prime*prime <= n; ++prime) {
-		if (n % prime == 0) {
-		    euler = euler / prime * (prime - 1);
-		    while (n % prime == 0) n /= prime;
-		}
+        if (n % prime == 0) {
+            euler = euler / prime * (prime - 1);
+            while (n % prime == 0) n /= prime;
+        }
     }
     return (n == 1 ? euler : euler / n * (n - 1));
 }
 
 int main()
 {
-	ll n;
-	while (1) {
-		scanf("%lld", &n);
-		if (!n) break;
-		printf("%lld\n", n == 1 ? 0 : euler_phi(n)); // ouput euler_phi(n) [n : 0 < n - 1]
-	}
+    ll n;
+    while (1) {
+        scanf("%lld", &n);
+        if (!n) break;
+        printf("%lld\n", n == 1 ? 0 : euler_phi(n)); // ouput euler_phi(n) [n : 0 < n - 1]
+    }
 }
