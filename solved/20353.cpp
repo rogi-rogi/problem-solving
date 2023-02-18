f34af27506f17234711e5151907bd1b304822a7b
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2023-02-18 16:42:21 +0900

    Create 20353.cpp

---
+++ solved/20353.cpp
@@ -0,0 +1,13 @@
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double area;
+    cin >> area;
+    cout << fixed;
+    cout.precision(8);
+    cout << sqrt(area) * 4;
+}
