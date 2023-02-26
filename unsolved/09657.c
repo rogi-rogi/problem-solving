4126e45cdecebd496705575d9f57b049b7a29974
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2023-02-26 23:58:24 +0900

    Create 09657.c

---
+++ unsolved/09657.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#define SIZE 1001
+
+int main()
+{
+    int dp[SIZE];
+    { // preprocessing
+        dp[1] = dp[3] = dp[4] = 1;
+        dp[2] = 0;
+        for (int i = 5; i < SIZE; ++i) {
+            if (dp[i - 1] + dp[i - 3] + dp[i - 4] == 3) dp[i] = 0;
+            else dp[i] = 1;
+            /*
+                i -1, i -3, i -4 번째 모두 상근이의 승리라면, i번째는 무조건 창영이가 이긴다.
+                1 0 1 1 1 1 0 1 0 1 1 1 1 0 1 0 1 1 1 1 0 1 0 1 1 1 1 0 1 0 1 1 1 1 0 1 반복구간 발생..
+            */
+        }
+    }
+    int N;
+    scanf("%d", &N);
+    for (int i = 0; i < 100; ++i) printf("%d ", dp[i]);
+    printf("%d", dp[N] == 1 ? "SK" : "CY");
+}
