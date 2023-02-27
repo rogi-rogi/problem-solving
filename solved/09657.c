04dee908e13e8d95452c5cb4829e32c518c9d4c3
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2023-02-27 19:52:33 +0900

    Update and rename unsolved/09657.c to solved/09657.c

---
+++ solved/09657.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int dp[7];
+    { // preprocessing
+        // dp[1] = dp[3] = dp[4] = 1;
+        // dp[2] = 0;
+        // for (int i = 5; i < SIZE; ++i) {
+        //     if (dp[i - 1] + dp[i - 3] + dp[i - 4] == 3) dp[i] = 0;
+        //     else dp[i] = 1;
+        //     /*
+        //         i -1, i -3, i -4 번째 모두 상근이의 승리라면, i번째는 무조건 창영이가 이긴다.
+        //     */
+        // }
+        dp[0] = dp[2] = dp[3] = dp[4] = dp[5] = 1;
+        dp[1] = dp[6] = 0;
+        // (1 2 3 4 5 6 7) (8 9 10 11 12 13 14) (15 16 17...)
+        // (1 0 1 1 1 1 0) (1 0 1  1  1  1  0 ) (1  0  1 ...) for toggling
+    }
+    int N;
+    scanf("%d", &N);
+    printf("%s", dp[(N - 1) % 7] == 1 ? "SK" : "CY");
+}
