d442090c30c9240a028d30e97cac4abe72c28bc2
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2023-03-05 00:00:46 +0900

    Update 01463.c

--- solved/01463.c
+++ solved/01463.c
@@ -7,9 +7,9 @@ int main()
 {
     { // preprocessing
         for (int i = 2; i < SIZE; ++i) {
-            dp[i] = dp[i - 1] + 1;                                          // (3) 
-	    if (i % 2 == 0 && dp[i / 2] + 1 < dp[i]) dp[i] = dp[i / 2] + 1; // (2)
-	    if (i % 3 == 0 && dp[i / 3] + 1 < dp[i]) dp[i] = dp[i / 3] + 1; // (1)
+            dp[i] = dp[i - 1] + 1;                                          // (3)
+            if (i % 2 == 0 && dp[i / 2] + 1 < dp[i]) dp[i] = dp[i / 2] + 1; // (2)
+            if (i % 3 == 0 && dp[i / 3] + 1 < dp[i]) dp[i] = dp[i / 3] + 1; // (1)
         }
     }
     int N;
