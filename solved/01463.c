4ed36d9444cf4272e316d56718c1ec9a110750c9
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2023-03-04 23:37:15 +0900

    Update 01463.c

--- solved/01463.c
+++ solved/01463.c
@@ -8,11 +8,11 @@ int main()
     { // preprocessing
         for (int i = 2; i < SIZE; ++i) {
             dp[i] = dp[i - 1] + 1;                                          // (3) 
-    		    if (i % 2 == 0 && dp[i / 2] + 1 < dp[i]) dp[i] = dp[i / 2] + 1; // (2)
-    		    if (i % 3 == 0 && dp[i / 3] + 1 < dp[i]) dp[i] = dp[i / 3] + 1; // (1)
+	    if (i % 2 == 0 && dp[i / 2] + 1 < dp[i]) dp[i] = dp[i / 2] + 1; // (2)
+	    if (i % 3 == 0 && dp[i / 3] + 1 < dp[i]) dp[i] = dp[i / 3] + 1; // (1)
         }
     }
-	  int N;
-	  scanf("%d", &N);
-	  printf("%d", dp[N]);
+    int N;
+    scanf("%d", &N);
+    printf("%d", dp[N]);
 }
