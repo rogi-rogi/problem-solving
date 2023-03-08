656d5f3afd7696375db63fd48e8409e0048a4a67
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2023-03-08 22:53:35 +0900

    Create 06826.py

---
+++ unsolved/06826.py
@@ -0,0 +1,6 @@
+weight = float(input())
+height = float(input())
+BMI = weight/(height * height)
+if BMI > 25 : print("Overweight")
+elif BMI > 18.5 : print("Normal weight")
+else : print("Underweight")
