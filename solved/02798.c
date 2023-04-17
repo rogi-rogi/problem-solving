867f6bc5f5ada1963f10bc4e960b76775983bb78
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2023-04-17 23:04:28 +0900

    Update 02798.c

--- solved/02798.c
+++ solved/02798.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
+#define SIZE 300001
+
+int cards[SIZE];
 
 int main()
 {
     int N, M;
-    scanf("%d %d", &N, &M);
-    int cards[N];
-    for(int i = 0; i < N; ++i)
-        scanf("%d", &cards[i]);
-    int max = 0;
-    for(int i = 0; i < N - 2; ++i)
-        for(int j = i + 1; j < N - 1; ++j)
-            for(int k = j + 1; k < N; ++k) {
+    { // input
+        scanf("%d %d", &N, &M);
+        for(int i = 0; i < N; ++i)
+            scanf("%d", &cards[i]);
+    }
+    
+    int max = 10;
+    for (int i = 0; i < N - 2; ++i) {
+        for (int j = i + 1; j < N - 1; ++j)
+            for (int k = j + 1; k < N; ++k) {
                 int sum = cards[i] + cards[j] + cards[k];
-                if(sum <= M && sum > max)
-                    max = sum;
-            }
+                if (sum <= M && sum > max) max = sum;
+            }        
+    }
     printf("%d\n", max);
 }
