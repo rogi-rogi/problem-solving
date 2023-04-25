5c43e58d9a4518e413e0ad61ba9ebbf699c63e28
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2023-04-25 11:03:26 +0900

    Create 01167.py

---
+++ solved/01167.py
@@ -0,0 +1,29 @@
+from collections import deque
+from sys import stdin
+input = stdin.readline
+
+def BFS(start, flag = 0) :
+    visited = [-1] * (V + 1)
+    queue = deque([start])
+    visited[start] = 0
+    while queue :
+        v1 = queue.popleft()
+        for v2, w in edges[v1] :
+            if visited[v2] == -1 :
+                visited[v2] = visited[v1] + w
+                queue.append(v2)
+    idx, dist = 0, 0
+    for i in range(1, V + 1) :
+        if dist < visited[i] :
+            dist = visited[i]
+            idx = i
+    return [dist, idx][flag == 0]
+
+if __name__ == "__main__" :
+    V = int(input())
+    edges = [[] for _ in range(V + 1)]
+    for _ in range(V) :
+        v1, *info = map(int, input().split())
+        for v2 in range(0, len(info) - 2, 2) :
+            edges[v1].append((info[v2], info[v2 + 1]))
+    print(BFS(BFS(1), 1))
