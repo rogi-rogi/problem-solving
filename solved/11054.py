b65f11a71afcd831e1cbd6cb1b89b67a2d493f77
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2023-04-28 23:28:47 +0900

    Create 11054.py

---
+++ solved/11054.py
@@ -0,0 +1,17 @@
+def LBS() : #Longest Bitonic Subsequence
+    for i in range(N) :
+        for j in range(i) :
+            if A[j] < A[i] and dp_LIS[j] == dp_LIS[i] :
+                dp_LIS[i] = dp_LIS[j] + 1
+    for i in range(N - 1, -1, -1) :
+        for j in range(N - 1, i, -1) :
+            if A[j] < A[i] and dp_LDS[j] == dp_LDS[i] :
+                dp_LDS[i] = dp_LDS[j] + 1
+
+if __name__ == "__main__" :
+    N = int(input())
+    A = [*map(int, input().split())]
+    dp_LIS = [1] * N
+    dp_LDS = [1] * N
+    LBS()
+    print(max([dp_LIS[i] + dp_LDS[i] for i in range(N)]) - 1)
