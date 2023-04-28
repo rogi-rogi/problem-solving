bb8187eecf5a45256368aad385123f5a8dccb746
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2023-04-29 03:19:39 +0900

    Update 11054.py

--- solved/11054.py
+++ solved/11054.py
@@ -1,9 +1,11 @@
-def LBS() : #Longest Bitonic Subsequence
+def LBS() : # Longest Bitonic Subsequence
+    # LIS
     for i in range(N) :
         for j in range(i) :
             if A[j] < A[i] and dp_LIS[j] == dp_LIS[i] :
                 dp_LIS[i] = dp_LIS[j] + 1
-    for i in range(N - 1, -1, -1) :
+    # LDS
+    for i in range(N - 1, -1, -1) : 
         for j in range(N - 1, i, -1) :
             if A[j] < A[i] and dp_LDS[j] == dp_LDS[i] :
                 dp_LDS[i] = dp_LDS[j] + 1
