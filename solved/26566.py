c67d956570bb8249734bed842ff65066fc7f5c41
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2023-05-28 18:12:33 +0900

    Create 26566.py

---
+++ solved/26566.py
@@ -0,0 +1,8 @@
+from math import pi
+
+for _ in range(int(input())) :
+    A, P1 = map(float, input().split())
+    R, P2 = map(float, input().split())
+    val1 = A / P1
+    val2 = (pi * R**2) / P2
+    print(["Slice of pizza", "Whole pizza"][val1 < val2])
