06280313797bb51d341ff8f87c1a4bdd174018d9
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2023-05-30 08:57:34 +0900

    Update 27590.py

--- solved/27590.py
+++ solved/27590.py
@@ -1,10 +1,10 @@
 ds, ys = map(int, input().split())
 dm, ym = map(int, input().split())
-sun_eclipse_year = ys - ds
-moon_eclipse_year = ym - dm
+sun_year = ys - ds
+moon_year = ym - dm
 
-while sun_eclipse_year != moon_eclipse_year :
-    if sun_eclipse_year < moon_eclipse_year :
-        sun_eclipse_year += ys
-    else : moon_eclipse_year += ym
-print(sun_eclipse_year)
+while sun_year != moon_year :
+    if sun_year < moon_year :
+        sun_year += ys
+    else : moon_year += ym
+print(sun_year)
