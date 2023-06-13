82042355691d04ac4751e6faeace7ae1a8dcf1dd
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2023-06-14 03:56:31 +0900

    Update 01298.py

--- solved/01298.py
+++ solved/01298.py
@@ -1,27 +1,31 @@
 from sys import stdin
 input = stdin.readline
 
-def BipartiteMatching() :
-    def DFS(v1) :
-        for v2 in edges[v1] :
-            if connected[v2] : continue
-            connected[v2] = True
-            if not path[v2] or DFS(path[v2]) :
-                path[v2] = v1
+def bipartite_matching() :
+    match = [0] * SIZE
+    visited = None
+    
+    def dfs(v1) :
+        visited[v1] = True
+        for v2 in graph[v1] :
+            if not match[v2] or not visited[match[v2]] and dfs(match[v2]) :
+                match[v2] = v1
                 return True
         return False
+        
     maximum_flow = 0
-    connected = None
-    path = [0] * (V + 1)
-    for i in range(1, V + 1) :
-        connected = [False] * (V + 1)
-        if DFS(i) : maximum_flow += 1
+    for i in range(1, SIZE) :
+        visited = [False] * SIZE
+        if dfs(i) :
+            maximum_flow += 1
+            if maximum_flow == N : break
     return maximum_flow
 
 if __name__ == "__main__" :
-    V, E = map(int, input().split())
-    edges = [[] for _ in range(V + 1)]
-    for _ in range(E) :
-        v1, v2 = map(int, input().split())
-        edges[v1].append(v2)
-    print(BipartiteMatching())
+    N, M = map(int, input().split())
+    SIZE = N + 1
+    graph = [[] for _ in range(N + 1)]
+    for _ in range(M) :
+        a, b = map(int, input().split())
+        graph[a].append(b)
+    print(bipartite_matching())
