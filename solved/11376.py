2aaa654be4a18b9fa517b65db4d1a823c553c953
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2023-06-14 02:40:34 +0900

    Update 11376.py

--- solved/11376.py
+++ solved/11376.py
@@ -1,35 +1,39 @@
-#pypy3
+# pypy3
 
-from sys import stdin
+from sys import setrecursionlimit, stdin
+setrecursionlimit(1002)
 input = stdin.readline
 
-def BipartiteMatching() :
-    def DFS(v1) :
-        for v2 in edges[v1] :
-            if visited[v2] : continue
-            visited[v2] = True
-            if not connected[v2] or DFS(connected[v2]) :
-                connected[v2] = v1
-                return True
+def bipartite_matching() :
+    match = [0] * SIZE
+    visited = None
+    
+    def dfs(v1) :
+        for v2 in graph[v1] :
+            if not visited[v2] :
+                visited[v2] = True
+                if not match[v2] or dfs(match[v2]) :
+                    match[v2] = v1
+                    return True
         return False
         
     maximum_flow = 0
-    visited = None
-    connected = [0] * (M + 1)
+    MAX = 2 * N
     for _ in range(2) :
         for i in range(1, N + 1) :
-            visited = [False] * (M + 1)
-            if DFS(i) :
+            visited = [False] * SIZE
+            if dfs(i) :
                 maximum_flow += 1
-                if maximum_flow == M : break
+                if maximum_flow == MAX : break
     return maximum_flow
 
 if __name__ == "__main__" :
     N, M = map(int, input().split())
-    edges = [[] for _ in range(N + 1)]
+    SIZE = 1001
+    graph = [[] for _ in range(N + 1)]
     for v1 in range(1, N + 1) :
         line = [*map(int, input().split())]
         if int(line[0]) >= 1 :
             _, *m = line
-            edges[v1] = m
-    print(BipartiteMatching())
+            graph[v1] = m
+    print(bipartite_matching())
