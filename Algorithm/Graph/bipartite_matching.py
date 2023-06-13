815b03afc45b11e4869d6e106668091993afbf1d
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2023-06-14 01:25:07 +0900

    Update bipartite_matching.py

--- Algorithm/Graph/bipartite_matching.py
+++ Algorithm/Graph/bipartite_matching.py
@@ -3,25 +3,15 @@ bipartite matching
 양쪽 그래프의 정점들을 매칭시키는 알고리즘.
 
 [Time Complexity] : O( VE ) = O( (N + M)E )
-N, M : 그룹 N, M의 인원 수
+N, M : 그룹 N, M의 인원 수, ( 1 <= N, M <= SIZE )
 E    : 그룹 N, M을 연결하는 간선의 수
 
-N <= M 일 때 성립한다.
-N > M 일떄는 두 그룹을 맞바꾸는 정석적인 방법과, 단순히 match의 크기를 N으로 하는 방법이 있다.
-후자의 경우 결과의 신뢰성과 알고리즘의 최적성이 보장되지 않는다.
-때문에 reverse_graph()를 사용해 역방향 그래프로 변환하고, N과 M도 맞바꾸어야 한다.
+기본적으로 N <= M 일 때 성립한다.
+때문에, N > M  일 때는 IndexError이 발생할 수 있으니 match, visited의 크기를 N과 M 그룹 중 더 큰 수를 기준으로 하자.
+배열의 크기를 늘리는 방법 이외에 탐색과정에서 N과 M의 대소비교에 따른 전처리를 통해서 IndexError가 발생하지 않게 할 수 있다.
 '''
-def reverse_graph(graph, N):
-    reversed_graph = [[] for _ in range(N + 1)]
-    for v1, edges in enumerate(graph):
-        for v2 in edges:
-            reversed_graph[v2].append(v1)
-    return reversed_graph
-
-def bipartite_matching(N, M, graph) :
-    if N > M :
-        graph = reverse_graph(graph, N)
-        N, M = M, N
+def bipartite_matching(N, M) :
+    if N > M : M = N # N <= M 으로 필요한 만큼 증가
     match = [0] * (M + 1) # 그룹 M의 i번째 정점에 대해 매칭된 그룹 N의 요소
     visited = None
     
@@ -37,7 +27,9 @@ def bipartite_matching(N, M, graph) :
     maximum_flow = 0
     for i in range(1, N + 1) :
         visited = [False] * (M + 1)
-        if dfs(i) : maximum_flow += 1
+        if dfs(i) : 
+            maximum_flow += 1
+            if maximum_flow == N : break # N개에 대해 maximun matching 성립, 더이상 탐색 불필요
     return maximum_flow
 
 if __name__ == "__main__" :
@@ -46,5 +38,5 @@ if __name__ == "__main__" :
     for _ in range(E) :
         v1, v2 = map(int, input().split())
         graph[v1].append(v2)
-    maximum_flow = bipartite_matching(N, M, graph)
+    maximum_flow = bipartite_matching(N, M)
     print(maximum_flow)
