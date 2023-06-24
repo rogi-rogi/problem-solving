a6e1985a5c1b4655507621e982d7161727a04fae
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2023-06-24 15:09:52 +0900

    Update FloydWarshall.py

--- Algorithm/Graph/FloydWarshall.py
+++ Algorithm/Graph/FloydWarshall.py
@@ -7,27 +7,33 @@ input = stdin.readline
 
 edges[v1][v2]  vs  edges[v1][t] + edges[t][v2]
 
-target : 양의 가중치를 갖는 순환 그래프, 음의 가중치를 가지는 비순환 그래프 
-모든 노드에 대한 최단 거리를 구할 때
-음의 가중치에 대해서는 자기 자신을 가리키는 가중치가 0보다 작은지를 판별해 음수 사이클을 벗어나야 한다.
-ex) (v1, v1) = 0 일 때, v1 -> v2 -> v3 < 0 인 경우 (v1, ..., v1) < 0 이 되므로 음수 사이클이 존재한다.  
+모든 정점에 대해 모든 정점까지의 최단거리를 구하기 위해 사용하며,
+음수 사이클이 없고, 양/음 가중치를 가지는 그래프에서 사용하는 알고리즘
 '''
 
-def FloydWarshall() :
+def floyd_warshall() :
     for t in range(1, V + 1) :
-        for v1 in range(1, V + 1) :
-            for v2 in range(1, V + 1) :
-                if v1 == v2 : edges[v1][v2] = 0 
-                w = edges[v1][t] + edges[t][v2]
-                if edges[v1][v2] > w : edges[v1][v2] = w
+        for u in range(1, V + 1) :
+            for v in range(1, V + 1) :
+                if u == v : continue
+                w = graph[u][t] + graph[t][v]
+                if graph[u][v] > w : graph[u][v] = w
 
 if __name__ == "__main__" :
     V, E = map(int, input().split())
-    edges = [[inf] * (V + 1) for _ in range(V + 1)]
+    graph = [[inf] * (V + 1) for _ in range(V + 1)]
+    # init 
+    for u in range(1, V + 1) :
+        graph[u][u] = 0
+    
+    # input
     for _ in range(E) :
-        v1, v2, w = map(int, input().split())
-        if edges[v1][v2] > w : edges[v1][v2] = w
-    FloydWarshall()
-    for v1 in range(1, V + 1) :
-        print(*[edges[v1][v2] for v2 in range(1, V + 1) if edges[v1][v2] != inf])
+        u, v, w = map(int, input().split())
+        # 동일한 길에 대해 가중치가 더 큰 길은 입력받을 필요가 없다.
+        if graph[u][v] > w : graph[u][v] = w
         
+    floyd_warshall()
+    
+    # output
+    for u in range(1, V + 1) :
+        print(*["%3d" % graph[u][v] for v in range(1, V + 1)])
