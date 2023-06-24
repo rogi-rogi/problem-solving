93fc52185a3dfd1e557a00da7645af68ceea0dd1
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2023-06-24 16:14:24 +0900

    Create 04344.c

---
+++ solved/04344.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int C, N, grade[1000];
+    scanf("%d", &C);
+    while (C--){
+        scanf("%d", &N);
+        double sum = 0, cnt = 0;
+        for (int i = 0; i < N; ++i) {
+            scanf("%d", &grade[i]);
+            sum += grade[i];
+        }
+        
+        double avg = sum / N;
+        for (int i = 0; i < N; ++i)
+            if (avg < grade[i]) cnt++;
+        double res = cnt * 100 / N;
+        double round45 = ((res*1000 - (int)(res*1000)) >= 0.5 ? 0.0001 : 0);
+        printf("%.3f%%\n", res + round45);
+    }
+}
