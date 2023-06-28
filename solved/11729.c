b9f8764650991ab4407eea390934e7f0566be947
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2023-06-28 18:20:27 +0900

    Update 11729.c

--- solved/11729.c
+++ solved/11729.c
@@ -3,18 +3,18 @@
 
 void hanoi(int n, int from, int to, int temp)
 {
-	if(n == 1) printf("%d %d\n", from, to);
-	else{
-		hanoi(n - 1, from, temp, to);
-		printf("%d %d\n", from, to);
-		hanoi(n - 1, temp, to, from);
-	}
+    if(n == 1) printf("%d %d\n", from, to);
+    else{
+        hanoi(n - 1, from, temp, to);
+        printf("%d %d\n", from, to);
+        hanoi(n - 1, temp, to, from);
+    }
 }
 
 int main()
 {
-	int n;
-	scanf("%d", &n);
-	printf("%d\n", (int)pow(2, n) - 1);
-	hanoi(n, 1, 3, 2);
+    int n;
+    scanf("%d", &n);
+    printf("%d\n", (int)pow(2, n) - 1);
+    hanoi(n, 1, 3, 2);
 }
