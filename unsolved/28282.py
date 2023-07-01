3fa3cb2a957e3fb730233d3246b945ba2ca6b3b9
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2023-07-01 12:34:40 +0900

    Create 28282.py

---
+++ unsolved/28282.py
@@ -0,0 +1,13 @@
+X, K = map(int, input().split())
+A = [*map(int, input().split())]
+matching = [False] * 10000
+half_size = len(A) >> 1
+res = 0
+for l in range(half_size) :
+    if not matching[A[l]] :
+        for r in range(half_size, len(A)) :
+            if A[l] != A[r] :
+                matching[A[l]] += 1
+        res += matching[A[l]]
+    else : res += matching[A[l]]
+print(res)
