f117b4c80757d6aedf2e81973933285f35414173
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2023-07-08 22:49:27 +0900

    Update 02206.java

--- unsolved/02206.java
+++ unsolved/02206.java
@@ -5,41 +5,43 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.StringTokenizer;
 
-public class BOJ02206 {
-    static int[] dx = {1, -1, 0, 0};
-    static int[] dy = {0, 0, 1, -1};
+public class Main {
+    static final int[] dx = {1, 0, -1, 0};
+    static final int[] dy = {0, 1, 0, -1};
     static int N, M;
-    static int[][] board;
-    static int[][][] visited;
+    static boolean[][] board;
+    static boolean[][][] visited;
     private static class Point {
-        private int wall, x, y;
-        Point(int wall, int x, int y) {
-            this.wall = wall;
+        private int x, y, dist, smash;
+        Point(int x, int y, int dist, int smash) {
             this.x = x;
             this.y = y;
+            this.dist = dist;
+            this.smash = smash;
         }
     }
 
     private static int bfs() {
-        visited[0][0][0] = 1;
+        visited[0][0][0] = true;
         Deque<Point> queue = new ArrayDeque<Point>();
-        queue.add(new Point(0, 0, 0));
+        queue.add(new Point(0, 0, 1, 0));
         while (!queue.isEmpty()) {
             Point p = queue.poll();
-            int wall = p.wall;
             int x = p.x;
             int y = p.y;
-            if (x == N - 1 && y == M - 1) return visited[wall][x][y];
+            int dist = p.dist;
+            int smash = p.smash;
+            if (x == N - 1 && y == M - 1) return dist;
             for (int i = 0; i < 4; ++i) {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
-                if (0 <= nx && nx < N && 0 <= ny && ny < M && visited[wall][nx][ny] == 0) {
-                    if (board[nx][ny] == 0) {
-                        queue.offer(new Point(wall, nx, ny));
-                        visited[wall][nx][ny] = visited[wall][x][y] + 1;
-                    } else if (wall == 0){ // && board[nx][ny] == 1
-                        queue.offer(new Point(1, nx, ny));
-                        visited[1][nx][ny] = visited[0][x][y] + 1;
+                if (0 <= nx && nx < N && 0 <= ny && ny < M && !visited[smash][nx][ny]) {
+                    if (board[nx][ny]) {
+                        queue.offer(new Point(nx, ny, dist + 1, smash));
+                        visited[smash][nx][ny] = true;
+                    } else if (smash == 0 && !visited[1][nx][ny]){ // && board[nx][ny] == 1
+                        queue.offer(new Point(nx, ny, dist + 1, 1));
+                        visited[1][nx][ny] = true;
                     }
                 }
             }
@@ -48,18 +50,19 @@ public class BOJ02206 {
     }
 
     public static void main(String[] args) throws IOException {
+        // init
         BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bfr.readLine());
 
         // input
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        board = new int[N][M];
-        visited = new int[2][N][M];
+        board = new boolean[N][M];
+        visited = new boolean[2][N][M];
         for (int i = 0; i < N; ++i) {
             String line = bfr.readLine();
             for (int j = 0; j < M; ++j)
-                board[i][j] = line.charAt(j) - '0';
+                board[i][j] = line.charAt(j) - '0' == 0;
         }
 
         // solution output
