6b82246bcf5f6e92a06e07413f3e6457550e59fd
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2023-09-02 13:24:05 +0900

    Create 26350.py

---
+++ solved/26350.py
@@ -0,0 +1,9 @@
+for _ in range(int(input())) :
+    N, *D = map(int, input().split())
+    res = False
+    for i in range(1, N) :
+        if (D[i-1] << 1) > D[i] :
+            break
+    else : res = True
+    print("Denominations:", *D)
+    print(["Bad coin denominations!", "Good coin denominations!"][res], '\n')
