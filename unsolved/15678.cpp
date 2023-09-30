c7f7ab8d4feacb40542c610fdd4b30cf6f5886c3
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2023-09-30 19:57:52 +0900

    Create 15678.cpp

---
+++ unsolved/15678.cpp
@@ -0,0 +1,48 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+typedef long long ll;
+
+vector<ll> tree, leaf;
+
+void update(int start, int end, int idx, int node = 1)
+{
+    if (end < idx || idx < start) return;
+    if (start == end) tree[node] = leaf[idx];
+    else {
+        int mid = (start + end)/2;
+        update(start, mid, idx, node*2);
+        update(mid+1, end, idx, node*2 +1);
+        tree[node] = max(tree[node*2], tree[node*2 +1]);
+    }
+}
+
+ll query(int start, int end, int i, int j, int node = 1)
+{
+    if (j < start || end < i) return -1000000000;
+    if (i <= start && end <= j) return tree[node];
+    int mid = (start + end)/2;
+    return max(query(start, mid, i, j, node*2), query(mid+1, end, i, j, node*2 +1));
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int N, D;
+    {
+        cin >> N >> D;
+        leaf.resize(N + 1);
+        for (int i = 1; i <= N; ++i) cin >> leaf[i];
+        int size = (1 << ((int)ceil(log2(N)) + 1));
+        tree.resize(size);
+    }
+    for (int i = 1; i <= N; ++i) {
+        leaf[i] = max(leaf[i], query(1, N, max(1, i - D), i - 1) + leaf[i]);
+        update(1, N, i);
+    }
+    cout << *max_element(leaf.begin() + 1, leaf.end());
+}
