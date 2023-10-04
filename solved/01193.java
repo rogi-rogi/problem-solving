96c8aac876bebcbb1641079d47ff6a56dc9ccafc
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2023-10-04 16:38:35 +0900

    Update 01193.java

--- solved/01193.java
+++ solved/01193.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class Main
 {
 	public static void main(String[] args) throws IOException{
-	    // init
+		// init
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	    StringBuilder sb = new StringBuilder();
 	    
