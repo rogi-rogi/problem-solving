532ca98faa6690c90a6b12fec832ea736565e76d
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2023-10-04 16:23:28 +0900

    Create 01193.java

---
+++ solved/01193.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) throws IOException{
+	    // init
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    StringBuilder sb = new StringBuilder();
+	    
+	    // input
+	    int N = Integer.parseInt(br.readLine());
+	    
+	    // solve
+	    {
+	        int depth = 1;
+    	    while (N > depth) {
+    	        N -= depth;
+    	        ++depth;
+    	    }
+    	    
+    	    int numerator, denominator;
+    	    if (depth % 2 == 0) {
+    	        numerator = N;
+    	        denominator = depth - N + 1;
+    	    } else {
+    	        numerator = depth - N + 1;
+    	        denominator = N;
+    	    }
+    	    
+    	    sb.append(numerator).append('/').append(denominator);
+	    }
+	    
+	    // output
+	    System.out.println(sb.toString());
+	}
+}
