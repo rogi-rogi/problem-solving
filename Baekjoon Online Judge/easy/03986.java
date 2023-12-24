34c3580d59377d6afaaaad355c206f381b905800
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2023-12-24 13:05:51 +0900

    Create 03986.java

---
+++ Baekjoon Online Judge/easy/03986.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // Init
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input;
+        Deque<Character> stack = new ArrayDeque<>();
+
+        // Input
+        int N = Integer.parseInt(br.readLine());
+        int cnt = 0;
+        while (N-- > 0) {
+            input = br.readLine();
+            final int SIZE = input.length();
+            // Solve
+            /*
+            for (char word : input.toCharArray()) {
+                if (!stack.isEmpty() && stack.peek().equals(word)) {
+                    stack.pop();
+                } else {
+                    stack.push(word);
+                }
+            }
+            */
+            for (int i = 0; i < SIZE; ++i) {
+                if (!stack.isEmpty() && stack.peek().equals(input.charAt(i))) {
+                    stack.pop();
+                } else {
+                    stack.push(input.charAt(i));
+                }
+            }
+            if (stack.isEmpty()) ++cnt;
+            stack.clear();
+        }
+
+        // Output
+        System.out.println(cnt);
+    }
+}
