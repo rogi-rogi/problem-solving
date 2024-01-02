ef8fc345ac7acf95af3fcc6e84ff7f277b04a2a7
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2024-01-02 16:06:09 +0900

    Create 04889.java

---
+++ Baekjoon Online Judge/easy/04889.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // Init
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        Deque<Character> stack = new ArrayDeque<>();
+        StringBuilder sb = new StringBuilder();
+
+        // Input
+        for (int i = 1; ; ++i) {
+            line = br.readLine();
+            if (line.charAt(0) == '-') break;
+
+            // Solve
+            for (Character c : line.toCharArray()) {
+                if (!stack.isEmpty() && stack.peekLast() == '{' && c == '}') {
+                    stack.pollLast();
+                } else stack.add(c);
+            }
+
+            int cnt = 0;
+            char L, R;
+            while (!stack.isEmpty()) {
+                L = stack.pop();
+                R = stack.pop();
+                if (L == '{') ++cnt;
+                else cnt += (R == '{' ? 2 : 1);
+            }
+            sb.append(i).append(". ").append(cnt).append("\n");
+        }
+
+        // Output
+        System.out.println(sb);
+    }
+}
