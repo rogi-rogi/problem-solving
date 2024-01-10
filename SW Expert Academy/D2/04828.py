2a4b10fef089a8e30e16b3cbb6a603837be31fe7
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2024-01-10 17:41:07 +0900

    Update 04828.py

--- SW Expert Academy/D2/04828.py
+++ SW Expert Academy/D2/04828.py
@@ -1,7 +1,3 @@
-'''
-[파이썬 S/W 문제해결 기본] 1일차 - min max
-https://swexpertacademy.com/main/learn/course/lectureProblemViewer.do
-'''
 for TC in range(1, int(input()) + 1):
     input()
     nums = [*map(int, input().split())]
