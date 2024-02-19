b131a2180c1b305b0949dfa0ac5713bb96d87bd1
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2024-02-19 15:33:59 +0900

    Create 12세 이하힌 여자 환자 목록 출력하기.sql

---
+++ Programmers/SQL High Score Kit/SELECT/12세 이하힌 여자 환자 목록 출력하기.sql
@@ -0,0 +1,7 @@
+-- 12세 이하힌 여자 환자 목록 출력하기
+-- https://school.programmers.co.kr/learn/courses/30/lessons/132201
+
+SELECT PT_NAME, PT_NO, GEND_CD, AGE, COALESCE(TLNO, "NONE") AS TLNO 
+FROM PATIENT AS P
+WHERE P.GEND_CD = 'W' AND P.AGE <= 12 
+ORDER BY P.AGE DESC, P.PT_NAME;
