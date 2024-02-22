941276964ae39af3519c728ca54f2fe1c4ea84ac
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2024-02-22 22:40:27 +0900

    Create 01(최소직사각형).py

---
+++ Programmers/Coding Test High Score Kit/Bruteforce/01(최소직사각형).py
@@ -0,0 +1,8 @@
+# 01(최소직사각형).py
+# https://school.programmers.co.kr/learn/courses/30/parts/12230
+def solution(sizes):
+    max_w, max_h = 0, 0
+    for w, h in sizes :
+        max_w = max(max_w, [w, h][h > w])
+        max_h = max(max_h, [h, w][h > w])
+    return max_w * max_h
