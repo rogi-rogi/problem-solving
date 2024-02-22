761d05ab2ef5883024ce274942c65f5e8a2d321d
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2024-02-22 23:21:01 +0900

    Create 01(K번째 수).py

---
+++ Programmers/Coding Test High Score Kit/Sort/01(K번째 수).py
@@ -0,0 +1,5 @@
+# 01(K번째 수).py
+# https://school.programmers.co.kr/learn/courses/30/lessons/42748
+
+def solution(array, commands):
+    return [sorted(array[i-1 : j])[k - 1] for i, j, k in commands]
