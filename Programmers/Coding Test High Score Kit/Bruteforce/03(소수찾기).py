cb952df407a93b7ecdc75164a1e81b7d0b24577b
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2024-02-23 00:56:41 +0900

    Create 03(소수찾기).py

---
+++ Programmers/Coding Test High Score Kit/Bruteforce/03(소수찾기).py
@@ -0,0 +1,24 @@
+# 03(소수찾기).py
+# https://school.programmers.co.kr/learn/courses/30/lessons/42839
+import math
+from itertools import permutations
+
+def is_prime(num):
+    if num < 2: return False
+    for i in range(2, int(math.sqrt(num)) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+def solution(numbers):
+    numbers = list(numbers)
+    N = len(numbers)
+    nums = set()
+    for i in range(1, N + 1):
+        for perm in permutations(numbers, i):
+            num = int(''.join(perm))
+            nums.add(num)
+    
+    primes = [num for num in nums if is_prime(num)]
+    answer = len(primes)
+    return answer
