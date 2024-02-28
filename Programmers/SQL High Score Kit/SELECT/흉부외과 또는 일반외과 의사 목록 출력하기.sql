d33602fb4ab279cc0f034c07232590bcbe14a18a
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2024-02-28 18:31:00 +0900

    Update and rename 04(흉부외과 또는 일반외과 의사 목록 출력하기).sql to 흉부외과 또는 일반외과 의사 목록 출력하기.sql

--- Programmers/SQL High Score Kit/SELECT/흉부외과 또는 일반외과 의사 목록 출력하기.sql
+++ Programmers/SQL High Score Kit/SELECT/흉부외과 또는 일반외과 의사 목록 출력하기.sql
@@ -1,7 +1,7 @@
--- 04(흉부외과 또는 일반외과 의사 목록 출력하기).sql
+-- 흉부외과 또는 일반외과 의사 목록 출력하기.sql
 -- https://school.programmers.co.kr/learn/courses/30/lessons/132203
 
 SELECT DR_NAME, DR_ID, MCDP_CD, DATE_FORMAT(HIRE_YMD, '%Y-%m-%d') AS HIRE_YMD
-FROM DOCTOR AS D
-WHERE D.MCDP_CD='CS' OR D.MCDP_CD='GS'
+FROM DOCTOR
+WHERE MCDP_CD='CS' OR MCDP_CD='GS'
 ORDER BY HIRE_YMD DESC, DR_NAME
