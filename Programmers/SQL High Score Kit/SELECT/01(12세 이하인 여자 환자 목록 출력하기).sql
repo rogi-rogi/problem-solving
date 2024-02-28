503338199648c7bd1da4ecabaf8f6f4f20a6752b
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2024-02-28 18:27:13 +0900

    Update and rename 01(12세 이하인 여자 환자 목록 출력하기).sql to 12세 이하인 여자 환자 목록 출력하기.sql

--- Programmers/SQL High Score Kit/SELECT/01(12세 이하인 여자 환자 목록 출력하기).sql
+++
@@ -1,7 +0,0 @@
--- 01(12세 이하인 여자 환자 목록 출력하기).sql
--- https://school.programmers.co.kr/learn/courses/30/lessons/132201
-
-SELECT PT_NAME, PT_NO, GEND_CD, AGE, COALESCE(TLNO, "NONE") AS TLNO 
-FROM PATIENT AS P
-WHERE P.GEND_CD = 'W' AND P.AGE <= 12 
-ORDER BY P.AGE DESC, P.PT_NAME;
