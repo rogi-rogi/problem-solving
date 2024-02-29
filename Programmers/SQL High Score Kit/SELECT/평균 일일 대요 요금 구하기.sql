c391d131a391b533ed7ba254efb8dca06bb489e2
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2024-02-29 18:01:45 +0900

    Create 평균 일일 대요 요금 구하기.sql

---
+++ Programmers/SQL High Score Kit/SELECT/평균 일일 대요 요금 구하기.sql
@@ -0,0 +1,6 @@
+-- 평균 일일 대요 요금 구하기.sql
+-- https://school.programmers.co.kr/learn/courses/30/lessons/151136
+
+SELECT ROUND(AVG(DAILY_FEE), 0) AS AVERAGE_FEE
+FROM CAR_RENTAL_COMPANY_CAR
+WHERE CAR_TYPE = 'SUV';
