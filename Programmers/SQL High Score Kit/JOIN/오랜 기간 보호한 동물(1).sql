ffa0989eceb73408464608cfed00d9e722d0dcab
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2024-03-02 11:12:47 +0900

    Create 오랜 기간 보호한 동물(1).sql

---
+++ Programmers/SQL High Score Kit/JOIN/오랜 기간 보호한 동물(1).sql
@@ -0,0 +1,8 @@
+-- 오랜 기간 보호한 동물(1).sql
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59044
+
+SELECT I.NAME, I.DATETIME
+FROM ANIMAL_INS AS I LEFT JOIN ANIMAL_OUTS AS O
+    ON I.ANIMAL_ID = O.ANIMAL_ID
+WHERE O.ANIMAL_ID IS NULL
+ORDER BY I.DATETIME LIMIT 3;
