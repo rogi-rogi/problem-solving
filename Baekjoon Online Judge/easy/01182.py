ceaf065dfaa44ff013f42d36a32136acc4570ef5
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2024-03-03 14:19:22 +0900

    Create 01182.py

---
+++ Baekjoon Online Judge/easy/01182.py
@@ -0,0 +1,26 @@
+'''
+def dfs(x, res) :
+    cnt = 0
+    if res == S and x > 0 :
+        cnt += 1
+    for i in range(x, N) :
+        if not visited[i] :
+            visited[i] = True
+            cnt += dfs(i + 1, res + nums[i])
+            visited[i] = False
+    return cnt
+'''
+def dfs(x, res) :
+    cnt = 0
+    if x == N :
+        return [0, 1][res == S]
+    cnt += dfs(x + 1, res)
+    cnt += dfs(x + 1, res + nums[x])
+    return cnt
+    
+    
+if __name__ == "__main__" :
+    N, S = map(int, input().split())
+    nums = [*map(int, input().split())]
+    # visited = [False] * N
+    print(dfs(0, 0) - [0, 1][S == 0])
