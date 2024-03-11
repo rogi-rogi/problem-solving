0a79dab6aad41716acd25e14147e52903011a820
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2024-03-11 17:35:40 +0900

    Create 03273.py

---
+++ Baekjoon Online Judge/easy/03273.py
@@ -0,0 +1,19 @@
+from itertools import combinations
+
+if __name__ == '__main__':
+    N = int(input())
+    nums = sorted([*map(int, input().split())])
+    X = int(input())
+    left, right = 0, N - 1
+    cnt = 0
+    while left < right:
+        temp = nums[left] + nums[right]
+        if temp < X:
+            left += 1
+        elif temp > X:
+            right -= 1
+        else :
+            left += 1
+            right -= 1
+            cnt += 1
+    print(cnt)
