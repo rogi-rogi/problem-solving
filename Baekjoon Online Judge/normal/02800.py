2b8d913861c506efd1a6ad5da3ebed5d35dee3f0
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2024-03-22 00:17:51 +0900

    Update 02800.py

--- Baekjoon Online Judge/normal/02800.py
+++ Baekjoon Online Judge/normal/02800.py
@@ -9,11 +9,11 @@ if __name__ == '__main__':
             stack.append(i)
         elif f[i] == ')':
             pair.append((stack.pop(), i))
-    res = []
+    res = set()
     for i in range(1, len(pair) + 1):
         for comb in combinations(pair, i):
             new_f = [*f]
             for left, right in comb:
                 new_f[left] = new_f[right] = ''
-            res.append(''.join(new_f))
-    print(*sorted(set(res)), sep='\n')
+            res.add(''.join(new_f))
+    print(*sorted(res), sep='\n')
