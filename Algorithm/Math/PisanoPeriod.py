7256f848f9337808719c65e22f3110c30d175a70
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2024-04-12 23:59:55 +0900

    Update PisanoPeriod.py

--- Algorithm/Math/PisanoPeriod.py
+++ Algorithm/Math/PisanoPeriod.py
@@ -3,5 +3,5 @@ def PisanoPeriod(N, MOD) : # return Pisano Period table
     if N <= 2 : return [0] + [1] * N
     T = [0, 1, 1]
     while T[-2] != 0 or T[-1] != 1 :
-        T.append((T[-2] + T[-1]) % MOD)
+        T.append((T[-2] + T[-1]) % MOD) 
     return T
