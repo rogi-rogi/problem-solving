90e0cfba0a96f8dd9f7b0cf0132d3103350e0053
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2024-04-19 14:52:19 +0900

    Create 10973.java

---
+++ Baekjoon Online Judge/easy/10973.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N;
+    static int[] nums;
+
+    private static boolean findPrevPermutation() {
+        int idx = N - 1;
+        while (idx > 0 && nums[idx - 1] < nums[idx]) --idx;
+        if (idx <= 0) return false;
+        int cur = N - 1;
+        while (nums[idx - 1] < nums[cur]) --cur;
+        swap(idx - 1, cur);
+        cur = N - 1;
+        while (idx < cur) swap(idx++, cur--);
+        return true;
+    }
+
+    private static void swap(int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public static void main(String[] args) throws IOException {
+        // Init
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        // Input
+        N = Integer.parseInt(br.readLine());
+        nums = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        // Solve
+        if (findPrevPermutation()) {
+            for (int i : nums)
+                sb.append(i).append(' ');
+        } else {
+            sb.append(-1);
+        }
+
+        // Output
+        System.out.println(sb);
+    }
+}
