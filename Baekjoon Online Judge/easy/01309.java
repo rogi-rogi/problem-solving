b2433671a76a9d58971db43098b3ddc7d1e9d8d2
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2024-04-23 00:27:31 +0900

    Update 01309.java

--- Baekjoon Online Judge/easy/01309.java
+++ Baekjoon Online Judge/easy/01309.java
@@ -8,12 +8,25 @@ public class Main {
         // Input
         final int N = Integer.parseInt(br.readLine());
         final int MOD = 9901;
-        
+
         // Solve
         int[] dp = new int[(int) 1e5 + 1];
+
+        /*
+        x(i), y(i) = z(i)
+
+        x(i + 1) = x(i) + 2y(i)
+        y(i + 1) = x(i) + y(i)
+
+        x(i + 1) = y(i + 1) + y(i)
+        x(i) = y(i) + y(i - 1)
+
+        y(i + 1) = 2y(i) + y(i - 1)
+        dp[i] = 2dp[i - 1] + dp[i - 2]
+        */
+        dp[0] = 1;
         dp[1] = 3;
-        dp[2] = 7;
-        for (int i = 3; i <= N; ++i) {
+        for (int i = 2; i <= N; ++i) {
             dp[i] = (2*dp[i-1] + dp[i-2]) % MOD;
         }
 
