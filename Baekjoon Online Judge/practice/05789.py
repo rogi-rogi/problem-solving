e246fe7a4325924e1248bc633f4559a44e959808
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2024-07-09 00:10:58 +0900

    Update 05789.py

--- Baekjoon Online Judge/practice/05789.py
+++ Baekjoon Online Judge/practice/05789.py
@@ -1,5 +1,8 @@
 if __name__ == '__main__':
     for _ in range(int(input())):
         num = input()
-        half = len(num) // 2 - 1
-        print(['Do-it-Not', 'Do-it'][num[half] == num[half + 1]])
+        res = 'Do-it'
+        if len(num) % 2 == 0:
+            half = len(num) // 2 - 1
+            res = ['Do-it-Not', 'Do-it'][num[half] == num[-half - 1]]
+        print(res)
