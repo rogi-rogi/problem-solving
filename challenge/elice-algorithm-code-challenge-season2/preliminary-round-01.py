bf27d23bd6bdaf97f2092c48aec43e1277fa9307
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2024-07-08 17:07:56 +0900

    Update preliminary-round-01.py

--- challenge/elice-algorithm-code-challenge-season2/preliminary-round-01.py
+++ challenge/elice-algorithm-code-challenge-season2/preliminary-round-01.py
@@ -1,5 +1,24 @@
+''' 
 from itertools import permutations
-
 N = input()
 INT_N = int(N)
 print(next(p for p in sorted(map(lambda p: int(''.join(p)), permutations(N))) if p > INT_N))
+'''
+
+def next_permutation(arr): # len(arr) >= 2
+    i = len(arr) - 1
+    while i >= 0 and arr[i - 1] >= arr[i]:
+        i -= 1
+    # i >= 1
+    
+    j = len(arr) - 1
+    while arr[i - 1] >= arr[j]:
+        j -= 1
+    
+    arr[i - 1], arr[j] = arr[j], arr[i - 1]
+
+    arr[i:] = reversed(arr[i:])
+    return arr
+
+N = [*map(int, input())]
+print(*next_permutation(N), sep='')
