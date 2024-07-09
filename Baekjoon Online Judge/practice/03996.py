0bd2f26b4f3b796a7eaeadfe7b86b801d8df8bc0
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2024-07-10 00:13:12 +0900

    Create 03996.py

---
+++ Baekjoon Online Judge/practice/03996.py
@@ -0,0 +1,26 @@
+from sys import stdin
+input = stdin.readline
+
+if __name__ == '__main__':
+    N, K = map(int, input().split())
+    digit = []
+    POW_K = K ** 2
+    while N > 0:
+        N, mod = divmod(N, POW_K)
+        digit.append(mod)
+
+    res = 0
+    flag = True
+    for i in range(len(digit) - 1, -1, -1):
+        if digit[i] < K:
+            res += digit[i]
+            res *= K
+        else:
+            res += K
+            res *= K ** (i + 1)
+            flag = False
+            break
+    res //= K
+    if flag:
+        res += 1
+    print(res)
