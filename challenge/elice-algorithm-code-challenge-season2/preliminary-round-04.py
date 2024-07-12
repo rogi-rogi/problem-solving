ab14f65e496235b85db8a6e2f9b6c6567e5695ab
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2024-07-12 17:09:39 +0900

    Update preliminary-round-04.py

--- challenge/elice-algorithm-code-challenge-season2/preliminary-round-04.py
+++ challenge/elice-algorithm-code-challenge-season2/preliminary-round-04.py
@@ -4,7 +4,7 @@ NO_CHILD = int(1e10)
 def dfs(cur, parent):
     diff = NO_CHILD
     
-    for child in edge[cur]:
+    for child in graph[cur]:
         if child == parent:
             continue
         dfs(child, cur)
@@ -16,11 +16,11 @@ def dfs(cur, parent):
 if __name__ == '__main__':
     N = int(input())
     dp = [None] * (N + 1) 
-    edge = defaultdict(list)
+    graph = defaultdict(list)
     for _ in range(N - 1):
         u, v = map(int, input().split())
-        edge[u].append(v)
-        edge[v].append(u)
+        graph[u].append(v)
+        graph[v].append(u)
     dfs(1, 0)
     res = [1 if dp[i] >= 0 else 0 for i in range(1, N + 1)]
     print(*res, sep='\n')
