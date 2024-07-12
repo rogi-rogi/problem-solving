6df4ac6182f809495cf4b7f14548d3ff00d33384
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2024-07-12 16:58:30 +0900

    Create preliminary-round-04.py

---
+++ challenge/elice-algorithm-code-challenge-season2/preliminary-round-04.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+NO_CHILD = int(1e12)
+
+def dfs(cur, parent):
+    diff = NO_CHILD
+    
+    for child in edge[cur]:
+        if child == parent:
+            continue
+        dfs(child, cur)
+        diff = min(diff, dp[child])
+
+    dp[cur] = cur - (0 if diff == NO_CHILD else diff)
+
+
+if __name__ == '__main__':
+    N = int(input())
+    dp = [None] * (N + 1) 
+    edge = defaultdict(list)
+    for _ in range(N - 1):
+        u, v = map(int, input().split())
+        edge[u].append(v)
+        edge[v].append(u)
+    dfs(1, 0)
+    res = [1 if dp[i] >= 0 else 0 for i in range(1, N + 1)]
+    print(*res, sep='\n')
