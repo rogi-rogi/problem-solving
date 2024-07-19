3c8d543cd01c2dd32d11aa9a272e97c9d80921e0
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2024-07-19 23:16:23 +0900

    Create 06321.java

---
+++ Baekjoon Online Judge/practice/06321.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // Init
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        // Input
+        final int N = Integer.parseInt(br.readLine());
+
+        for (int no = 1; no <= N; ++no) {
+            char[] name = br.readLine().toCharArray();
+            // Solve
+            for (int i = 0; i < name.length; ++i) {
+                name[i] = (char) ((name[i] - 'A' + 1) % 26 + 'A');
+            }
+            sb.append("String #").append(no).append("\n");
+            sb.append(name).append("\n\n");
+        }
+
+        // Output
+        System.out.println(sb);
+    }
+}
