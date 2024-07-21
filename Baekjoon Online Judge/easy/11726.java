fe6ae8270a536a95599d0a26a10cd3597e2b5e20
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2024-07-21 16:32:08 +0900

    Create 11726.java

---
+++ Baekjoon Online Judge/easy/11726.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class Main {
+    final static int MOD = (int) (1e4 + 7);
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        // Init
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // Input
+        final int N = Integer.parseInt(br.readLine());
+        dp = new int[N + 1];
+
+        // Solve
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= N; ++i) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
+        }
+
+        // Output
+        System.out.println(dp[N]);
+    }
+}
