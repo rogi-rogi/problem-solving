525e2bcd58d910dd4cf91f80110d6d104a3c68ab
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2024-07-21 16:09:35 +0900

    Create 06378.java

---
+++ Baekjoon Online Judge/practice/06378.java
@@ -0,0 +1,32 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // Init
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        // Input
+        while (true) {
+            String N = br.readLine();
+            if (N.equals("0")) break;
+            int num = 0;
+            int SIZE = N.length();
+            for (int i = 0; i < SIZE; ++i) {
+                num += N.charAt(i) - '0';
+            }
+            // Output
+            System.out.println(getDigitalRoot(num));
+        }
+    }
+    
+    private static int getDigitalRoot(int num) {
+        if (num < 10) return num;
+        int res = 0;
+        while (num > 0) {
+            res += num % 10;
+            num /= 10;
+        }
+        return getDigitalRoot(res);
+    }
+}
