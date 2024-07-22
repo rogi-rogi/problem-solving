46eafb2658bd748259d6cdde0ff671a5e3594323
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2024-07-23 01:11:07 +0900

    Update 43068.py

--- goorm/L1/43068.py
+++ goorm/L1/43068.py
@@ -1,7 +1,7 @@
 for _ in range(int(input())):
-  X, Y, N = map(int, input().split())
-	d = abs(X) + abs(Y)
-	if d == N or (d < N and (d - N) % 2 == 0):
-		print("YES")
-	else:
-		print("NO")
+    X, Y, N = map(int, input().split())
+    d = abs(X) + abs(Y)
+    if d == N or (d < N and (d - N) % 2 == 0):
+        print("YES")
+    else:
+        print("NO")
