b5b31a48e96d3890c749b698d58e63773b73437a
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2024-07-23 18:07:24 +0900

    Update 01158.py

--- solved/01158.py
+++ solved/01158.py
@@ -1,26 +1,17 @@
-#default ver
-from collections import deque
+from sys import stdin
+input = stdin.readline
 
-N, K = map(int, input().split())
-queue = deque([i for i in range(1, N + 1)])
-result = []
-cnt = 0
-while queue :
-    cnt += 1
-    temp = queue.popleft()
-    if cnt == K :
-        cnt = 0
-        result.append(temp)
-    else : queue.append(temp)
-print('<'+str(result)[1:-1]+'>')
+if __name__ == '__main__':
+    # Input
+    N, K = map(int, input().split())
+    queue = [*range(1, N + 1)]
+    res = []
 
+    # Solve
+    idx = 0
+    while queue:
+        idx = (idx + K - 1) % len(queue)
+        res.append(queue.pop(idx))
 
-#fast ver
-N, K = map(int, input().split())
-queue = list(range(1, N + 1))
-result = []
-idx = 0
-while queue:
-    idx = (idx + K - 1) % len(queue)
-    result.append(queue.pop(idx))
-print('<'+str(result)[1:-1]+'>')
+    # Output
+    print('<' + str(res)[1:-1] + '>')
