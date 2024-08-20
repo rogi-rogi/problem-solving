df463c0bd609118186bf3f704dc1a89485a792b7
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2024-08-21 02:08:42 +0900

    Update 02240.py

--- Baekjoon Online Judge/normal/02240.py
+++ Baekjoon Online Judge/normal/02240.py
@@ -8,11 +8,10 @@ if __name__ == '__main__':
 
     # Solve
     dp = [[0] * (W + 1) for _ in range(T + 1)]
-    dp[1][0] = jadu[1] % 2
-    dp[1][1] = jadu[1] // 2
-    for t in range(2, T + 1):
-        for w in range(W + 1):
-            dp[t][w] = max(dp[t-1][:w + 1]) + (jadu[t] % 2 if w % 2 == 0 else jadu[t] // 2)
+    for t in range(1, T + 1):
+        dp[t][0] = dp[t - 1][0] + (1 if jadu[t] % 2 == 1 else 0)
+        for w in range(1, W + 1):
+            dp[t][w] = max(dp[t - 1][w - 1], dp[t - 1][w]) + (jadu[t] % 2 if w % 2 == 0 else jadu[t] // 2)
 
     # Output
     print(max(dp[T]))
