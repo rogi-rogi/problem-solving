f27ee1c048b4adea4684ac9e71b05d2e1b067449
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2024-08-23 01:24:19 +0900

    Create 02 계단 오르기(climbing-stairs).py

---
+++ Code Tree/Algorithm Intro/DP I/02 계단 오르기(climbing-stairs).py
@@ -0,0 +1,15 @@
+# Input
+SIZE = 1001
+MOD = int(1e4) + 7
+
+N = int(input())
+
+# Solve
+dp = [0] * SIZE
+dp[2] = 1
+dp[3] = 1
+for i in range(4, SIZE):
+    dp[i] = (dp[i - 2] + dp[i - 3]) % MOD
+
+# Output
+print(dp[N])
