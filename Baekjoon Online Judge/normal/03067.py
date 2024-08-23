3d5efa56e49dcd5712640b86c952650dbc82ae90
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2024-08-23 17:00:32 +0900

    Create 03067.py

---
+++ Baekjoon Online Judge/normal/03067.py
@@ -0,0 +1,20 @@
+from sys import stdin
+input = stdin.readline
+
+if __name__ == '__main__':
+    # Input
+    for _ in range(int(input())):
+        N = int(input())
+        coins = [0, *map(int, input().split())]
+        M = int(input())
+
+        # Solve
+        dp = [0] * (M+1)
+        dp[0] = 1
+
+        for coin in coins[1:]:
+            for m in range(coin, M + 1):
+                dp[m] += dp[m - coin]
+
+        # Output
+        print(dp[M])
