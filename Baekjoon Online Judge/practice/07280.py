19b37f399b319e5dfd0808a4c15f317162b9ae75
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2024-09-03 20:26:40 +0900

    Update 07280.py

--- Baekjoon Online Judge/practice/07280.py
+++ Baekjoon Online Judge/practice/07280.py
@@ -2,10 +2,15 @@ from sys import stdin
 input = stdin.readline
 
 if __name__ == '__main__':
+    # Input
     res = sorted([input().split() for _ in range(51)], key=lambda x: (x[0], int(x[1])))
+    
+    # Solve
     res.append(['S', 13])
     for i in range(4):
         for j in range(1, 14):
             if res[i*13 + j - 1][1] != str(j):
+                
+                # Output
                 print("BKSV"[i], j)
                 exit(0)
