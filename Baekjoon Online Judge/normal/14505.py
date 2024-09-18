9dcec0df091cc0a8d7b49d9bfc127504de8138e4
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2024-09-18 17:44:25 +0900

    Update 14505.py

--- Baekjoon Online Judge/normal/14505.py
+++ Baekjoon Online Judge/normal/14505.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     for l in range(1, N + 1):
         for r in range(l, N + 1):
             if S[l] == S[r]:
-                dp[r] = dp[r] + prefix_sum_dp[N] - prefix_sum_dp[r] + 1
+                dp[r] += prefix_sum_dp[N] - prefix_sum_dp[r] + 1
             prefix_sum_dp[r] = prefix_sum_dp[r - 1] + dp[r]
 
     # Output
