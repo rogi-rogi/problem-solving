8755037cc94c23024456bf8fc1f31ef2dbf5c91f
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2024-11-26 09:09:48 +0900

    Update 24444.java

--- Baekjoon Online Judge/easy/24444.java
+++ Baekjoon Online Judge/easy/24444.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Main {
     static int N;
-    static List<List<Integer>> graph;
+    static List<NavigableSet<Integer>> graph;
     public static void main(String[] args) throws IOException {
         // Init
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,20 +16,17 @@ public class Main {
         final int R = Integer.parseInt(input[2]);
         graph = new ArrayList<>(N + 1);
         for (int i = 0; i <= N; ++i) {
-            graph.add(new ArrayList<>());
+            graph.add(new TreeSet<>());
         }
         for (int i = 0; i < M; ++i) {
             input = br.readLine().split(" ");
             int u = Integer.parseInt(input[0]);
             int v = Integer.parseInt(input[1]);
-          
             graph.get(u).add(v);
             graph.get(v).add(u);
         }
 
         // Solve
-        graph.forEach(Collections::sort);
-
         int[] visited = bfs(R);
         for (int i = 1; i <= N; ++i) {
             sb.append(visited[i]).append('\n');
