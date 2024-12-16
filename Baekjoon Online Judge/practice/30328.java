d230895ae083b2eff884324b8e1b7efa582484b3
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2024-12-17 00:49:17 +0900

    Create 30328.java

---
+++ Baekjoon Online Judge/practice/30328.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // Init
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // Input
+        final int N = Integer.parseInt(br.readLine());
+
+        // Solve & Output
+        System.out.println(N * 4000);
+    }
+}
