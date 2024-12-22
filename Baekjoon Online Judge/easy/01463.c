9d825be0f4e64917f297d2be3de1de09d39fb5a2
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2024-12-23 04:01:06 +0900

    Update 01463.c

--- Baekjoon Online Judge/easy/01463.c
+++ Baekjoon Online Judge/easy/01463.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #define SIZE 1000001
 
-int main()
-{
+int main() {
     // Init
     int N;
     int dp[SIZE];
@@ -13,10 +12,12 @@ int main()
     // Solve
     for (int i = 2; i < SIZE; ++i) {
         dp[i] = dp[i - 1] + 1;
-        if (i % 2 == 0 && dp[i / 2] + 1 < dp[i])
+        if (i % 2 == 0 && dp[i / 2] + 1 < dp[i]) {
             dp[i] = dp[i / 2] + 1;
-        if (i % 3 == 0 && dp[i / 3] + 1 < dp[i])
+        }
+        if (i % 3 == 0 && dp[i / 3] + 1 < dp[i]) {
             dp[i] = dp[i / 3] + 1;
+        }
     }
     
     // Output
