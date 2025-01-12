c81eecd70a5499ab99a707f56e5f8e2dd9a18824
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2025-01-13 05:41:37 +0900

    Create 11437.java

---
+++ Baekjoon Online Judge/normal/11437.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static List<List<Integer>> tree;
+    static int[] parent, depth;
+
+    private static void initTree(int node, int parentNode) {
+        parent[node] = parentNode;
+        depth[node] = depth[parentNode] + 1;
+        for (int child : tree.get(node)) {
+            if (child != parentNode) {
+                initTree(child, node);
+            }
+        }
+    }
+
+    private static int LCA(int u, int v) {
+        while (depth[u] > depth[v]) {
+            u = parent[u];
+        }
+        while (depth[v] > depth[u]) {
+            v = parent[v];
+        }
+        while (u != v) {
+            u = parent[u];
+            v = parent[v];
+        }
+        return u;
+    }
+
+    public static void main(String[] args) throws IOException {
+        // Init
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        // Input
+        int N = Integer.parseInt(br.readLine());
+        tree = new ArrayList<>();
+        for (int i = 0; i <= N; ++i) {
+            tree.add(new ArrayList<>());
+        }
+        for (int i = 0; i < N - 1; ++i) {
+            st = new StringTokenizer(br.readLine());
+            final int u = Integer.parseInt(st.nextToken());
+            final int v = Integer.parseInt(st.nextToken());
+            tree.get(u).add(v);
+            tree.get(v).add(u);
+        }
+
+
+        parent = new int[N + 1];
+        depth = new int[N + 1];
+        initTree(1, 0);
+
+        int M = Integer.parseInt(br.readLine());
+        while (M-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            final int u = Integer.parseInt(st.nextToken());
+            final int v = Integer.parseInt(st.nextToken());
+
+            // Solve
+            sb.append(LCA(u, v)).append('\n');
+        }
+
+        // Output
+        System.out.println(sb);
+    }
+}
