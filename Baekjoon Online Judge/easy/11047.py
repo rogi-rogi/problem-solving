ca869b5b6f2730ae93a39012c4f4d6fa5a6a024a
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2025-01-29 12:08:32 +0900

    Create 11047.py

---
+++ Baekjoon Online Judge/easy/11047.py
@@ -0,0 +1,21 @@
+from sys import stdin
+
+input = stdin.readline
+
+if __name__ == '__main__':
+    # Init
+    res = []
+
+    # Input
+    N, K = map(int, input().split())
+
+    # Solve
+    coins = sorted([int(input()) for _ in range(N)], reverse=True)
+    cnt = 0
+    for coin in coins:
+        if K >= coin:
+            cnt += K // coin
+            K %= coin
+
+    # Output
+    print(cnt)
