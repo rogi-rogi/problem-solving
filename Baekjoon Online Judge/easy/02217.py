1897eae2f7bf65db040b7c2579d96cc6a613fc42
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2025-01-29 12:31:47 +0900

    Update 02217.py

--- Baekjoon Online Judge/easy/02217.py
+++ Baekjoon Online Judge/easy/02217.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     # Solve
     res = 0
     rope_list = sorted([int(input()) for _ in range(N)], reverse=True)
-    for idx, rope  in enumerate(rope_list):
+    for idx, rope in enumerate(rope_list):
         res = max(res, rope * (idx + 1))
 
     # Output
