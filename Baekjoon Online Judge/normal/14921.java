1211e0d8b9bb2a133233e31a00513f1fe3a8c4c3
rogi-rogi <yrkim6883@gmail.com>
2025-02-05 19:15:46 +0900

    solve boj 14921

---
+++ Baekjoon Online Judge/normal/14921.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // Init
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // Input
+        final int N = Integer.parseInt(br.readLine());
+        int[] A = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        // Solve
+        Arrays.sort(A);
+        int left = 0, right = N - 1;
+        int min = (int) 2e8, B = 0;
+        while (left < right) {
+            int sumAB = A[left] + A[right];
+            if (Math.abs(sumAB) < min) {
+                min = Math.abs(sumAB);
+                B = sumAB;
+            }
+            if (sumAB > 0) {
+                --right;
+            } else {
+                ++left;
+            }
+        }
+
+        // Output
+        System.out.print(B);
+    }
+}
