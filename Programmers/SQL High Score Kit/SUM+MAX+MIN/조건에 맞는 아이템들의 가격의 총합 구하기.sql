b488481cbd46d9564d80e87e1fe95525573543ec
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2025-02-20 10:30:12 +0900

    Update 조건에 맞는 아이템들의 가격의 총합 구하기.sql

--- Programmers/SQL High Score Kit/SUM+MAX+MIN/조건에 맞는 아이템들의 가격의 총합 구하기.sql
+++ Programmers/SQL High Score Kit/SUM+MAX+MIN/조건에 맞는 아이템들의 가격의 총합 구하기.sql
@@ -2,5 +2,5 @@
 -- https://school.programmers.co.kr/learn/courses/30/lessons/273709
 
 SELECT SUM(PRICE) AS 'TOTAL_PRICE'
-FROM ITEM_INFO
-WHERE RARITY = 'LEGEND';
+  FROM ITEM_INFO
+ WHERE RARITY = 'LEGEND';
