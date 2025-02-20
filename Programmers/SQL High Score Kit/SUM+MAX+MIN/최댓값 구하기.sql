be8073e5abf65fe08ab954a2e56a6251ce916d1d
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2025-02-20 09:53:54 +0900

    Update 최댓값 구하기.sql

--- Programmers/SQL High Score Kit/SUM+MAX+MIN/최댓값 구하기.sql
+++ Programmers/SQL High Score Kit/SUM+MAX+MIN/최댓값 구하기.sql
@@ -1,7 +1,5 @@
 -- 최댓값 구하기.sql
 -- https://school.programmers.co.kr/learn/courses/30/lessons/59415
 
-SELECT DATETIME AS '시간'
-FROM ANIMAL_INS
-ORDER BY DATETIME DESC
-LIMIT 1;
+SELECT MAX(DATETIME) AS '시간'
+  FROM ANIMAL_INS;
