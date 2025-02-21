7c8feeca8205c751650d73f4a66f4726c3192d46
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2025-02-21 13:08:22 +0900

    Update 가장 큰 수.java]

--- Programmers/Coding Test High Score Kit/Sort/가장 큰 수.java
+++ Programmers/Coding Test High Score Kit/Sort/가장 큰 수.java
@@ -7,7 +7,7 @@ class Solution {
     public String solution(int[] numbers) {
         String[] arr = Arrays.stream(numbers).mapToObj(String::valueOf).toArray(String[]::new);
         Arrays.sort(arr, (a, b) -> (b+a).compareTo(a+b));
-        String res = String.join("", arr).replaceFirst("^0*", "");
-        return res.isEmpty() ? "0" : res;
+        String res = String.join("", arr);
+        return res.charAt(0) == '0' ? "0" : res;
     }
 }
