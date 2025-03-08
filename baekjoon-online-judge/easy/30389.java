a7d20cb9a4a9d1a86082cb673f58a8a876aa8086
rogi-rogi <yrkim6883@gmail.com>
2025-03-08 15:11:19 +0900

    solve boj 30389

---
+++ baekjoon-online-judge/easy/30389.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static Set<String> getSuffixes(String s) {
+        StringBuilder sb = new StringBuilder();
+        Set<String> suffixes = new HashSet<>();
+        for (int i = s.length() -1; i >= 0; --i) {
+            sb.append(s.charAt(i));
+            suffixes.add(sb.toString());
+        }
+        return suffixes;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // Input
+        int N = Integer.parseInt(br.readLine());
+        Set<String> xorSet = new HashSet<>();
+        while (N-- > 0) {
+
+            // Solve
+            for (String suffix : getSuffixes(br.readLine())) {
+                if (!xorSet.add(suffix)) {
+                    xorSet.remove(suffix);
+                }
+            }
+        }
+
+        // Output
+        System.out.println(xorSet.size());
+    }
+}
