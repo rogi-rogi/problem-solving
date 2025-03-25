4ba327cd44295dd698e0626941338b2428437eda
rogi-rogi <yrkim6883@gmail.com>
2025-03-26 02:35:10 +0900

    solve boj 13022

--- baekjoon-online-judge/easy/13022.java
+++ baekjoon-online-judge/easy/13022.java
@@ -1,48 +1,35 @@
 import java.io.*;
 
 public class Main {
-    public static boolean check(String word) {
-        String[] patterns = new String[13];
-        for (int i = 1; i <= 12; i++) {
-            StringBuilder sb = new StringBuilder(4 * i);
-            for (int j = 0; j < i; j++) {
-                sb.append('w');
-            }
-            for (int j = 0; j < i; j++) {
-                sb.append('o');
-            }
-            for (int j = 0; j < i; j++) {
-                sb.append('l');
-            }
-            for (int j = 0; j < i; j++) {
-                sb.append('f');
-            }
-            patterns[i] = sb.toString();
-        }
+    static char[] ptn = "olf".toCharArray();
 
-        int idx = 0;
-        int n = word.length();
-        while (idx < n) {
-            boolean matched = false;
-            for (int i = 1; i <= 12; i++) {
-                int patLen = 4 * i;
-                if (idx + patLen > n) break;
-                if (word.startsWith(patterns[i], idx)) {
-                    idx += patLen;
-                    matched = true;
-                    break;
+    private static boolean check(char[] S) {
+        int idx = 0, gap = 0;
+        while (idx < S.length) {
+            if (S[idx] != 'w') {
+                return false;
+            }
+            while (idx < S.length && S[idx] == 'w') {
+                ++gap;
+                ++idx;
+            }
+            for (char ch : ptn) {
+                for (int g = 0; g < gap; ++g) {
+                    if (idx >= S.length || S[idx] != ch) {
+                        return false;
+                    }
+                    ++idx;
                 }
             }
-            if (!matched) return false;
+            gap = 0;
         }
         return true;
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
         // Input
-        String S = br.readLine();
+        char[] S = br.readLine().toCharArray();
 
         // Solve & Output
         System.out.println(check(S) ? 1 : 0);
