1f8799e0b9a9017d1ced1ded15631b655743e88a
rogi-rogi <yrkim6883@gmail.com>
2025-06-19 02:48:08 +0900

    solve Code Tree 4-5-11

---
+++ code-tree/Algorithm Intro/DP I/11 정수 사각형 차이의 최소 2(challenge-minimum-difference-on-the-integer-grid-2).py
@@ -0,0 +1,26 @@
+'''
+https://www.codetree.ai/ko/trails/complete/curated-cards/challenge-minimum-difference-on-the-integer-grid-2/description
+'''
+
+N = int(input())
+grid = [None] + [[0, *map(int, input().split())] for _ in range(N)]
+
+# Please write your code here.
+
+INF = int(1e10)
+dp = [[[INF] * 101 for _ in range(N + 1)] for _ in range(N + 1)]
+
+dp[1][1][grid[1][1]] = grid[1][1]
+for i in range(1, N + 1):
+    for j in range(1, N + 1):
+        for k in range(1, 101):
+            min_k = min(k, grid[i][j])
+            dp[i][j][min_k] = min(dp[i][j][min_k],
+                                max(grid[i][j],
+                                    min(dp[i - 1][j][k], dp[i][j - 1][k])))
+
+res = INF
+for k in range(1, 101):
+    if dp[N][N][k] != INF and dp[N][N][k] - k < res:
+        res = dp[N][N][k] - k
+print(res)
