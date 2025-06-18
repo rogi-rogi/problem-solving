43690f00e851672dcc217af80b2b8fa43e4216d0
rogi-rogi <yrkim6883@gmail.com>
2025-06-19 00:24:32 +0900

    solve Code Tree 4-5-7

--- code-tree/Algorithm Intro/DP I/07 정수 사각형 최대 합(maximum-sum-path-in-square).py
+++ code-tree/Algorithm Intro/DP I/07 정수 사각형 최대 합(maximum-sum-path-in-square).py
@@ -1,19 +1,16 @@
 '''
-https://www.codetree.ai/missions/2/problems/maximum-sum-path-in-square?&utm_source=clipboard&utm_medium=text
+https://www.codetree.ai/ko/trails/complete/curated-cards/intro-maximum-sum-path-in-square/description
 '''
 
-# Init
-SIZE = 101
-
-# Input
 N = int(input())
-matrix = [None] + [[0, *map(int, input().split())] for _ in range(N)]
-dp = [[0] * SIZE for _ in range(SIZE)]
+grid = [list(map(int, input().split())) for _ in range(N)]
 
-# Solve
+# Please write your code here.
+
+dp = [[0] * (N + 1) for _ in range(N + 1)]
+
 for i in range(1, N + 1):
     for j in range(1, N + 1):
-        dp[i][j] = matrix[i][j] + max(dp[i - 1][j], dp[i][j - 1])
+        dp[i][j] = grid[i - 1][j - 1] + max(dp[i - 1][j], dp[i][j - 1])
 
-# Output
 print(dp[N][N])
