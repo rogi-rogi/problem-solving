0c43e8091e6c262e6a80561adc90c03d80905148
rogi-rogi <yrkim6883@gmail.com>
2025-06-20 17:27:29 +0900

    solve Code Tree 4-5-19

---
+++ code-tree/Algorithm Intro/DP I/19 알바로 부자 되기(test-being-rich-by-working-part-time).py
@@ -0,0 +1,22 @@
+'''
+https://www.codetree.ai/ko/trails/complete/curated-cards/test-being-rich-by-working-part-time/description
+'''
+
+n = int(input())
+jobs = [tuple(map(int, input().split())) for _ in range(n)]
+
+jobs.sort(lambda x : x[1])
+s = [job[0] for job in jobs]
+e = [job[1] for job in jobs]
+p = [job[2] for job in jobs]
+
+# Please write your code here.
+
+dp = [0] * n
+dp[0] = p[0]
+for i in range(n):
+    for j in range(i):
+        if e[j] < s[i]:
+            dp[i] = max(dp[i], dp[j] + p[i])
+
+print(max(dp))
