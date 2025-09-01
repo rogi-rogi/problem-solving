307b0c6c045c04e3e1b9bf075ee9ba2b45e3ecf3
rogi-rogi <yrkim6883@gmail.com>
2025-09-02 00:13:50 +0900

    solve BOJ 33943

--- baekjoon-online-judge/normal/33943.java
+++ baekjoon-online-judge/normal/33943.java
@@ -7,9 +7,11 @@ public class Main {
     static int[] P;
 
     private static int bfs() {
+        if (N == 0 && M != 0) return -1;
         Deque<int[]> queue = new ArrayDeque<>();
         queue.add(new int[]{ 0, 0 });
         int[] visited = new int[20001];
+        Arrays.fill(visited, (int) 2e4);
         visited[10000] = 0;
 
         while (!queue.isEmpty()) {
@@ -22,7 +24,7 @@ public class Main {
             for (int i = 0; i < N; ++i) {
                 final int nextN = n + 1;
                 final int nextM = m + P[i];
-                if (nextM >= -10000 && nextM <= 10000) {
+                if (nextM >= -10000 && nextM <= 10000 && visited[nextM + 10000] > nextN) {
                     queue.add(new int[] { nextN, nextM });
                     visited[nextM + 10000] = nextN;
                 }
@@ -39,11 +41,14 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        P = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        if (N > 0) {
+            P = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
-        // Solve
-        Arrays.sort(P);
+            // Solve
+            Arrays.sort(P);
 
-        System.out.println(bfs());
+            // Output
+            System.out.println(bfs());
+        }
     }
 }
