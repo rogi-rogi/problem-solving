153c001d70956e23c36c0511e81e7038d072d394
rogi-rogi <yrkim6883@gmail.com>
2025-10-30 02:40:16 +0900

    solve BOJ 30455

---
+++ baekjoon-online-judge/practice/30455.java
@@ -0,0 +1,15 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		// Input
+		final int N = Integer.parseInt(br.readLine());
+
+		// Solve & Output
+		System.out.println(N % 2 == 0 ? "Duck" : "Goose");
+	}
+}
