7f41bd82f33f95b78b870b99b25b55fd6b5f44a6
rogi-rogi <yrkim6883@gmail.com>
2025-11-29 01:03:35 +0900

    boj: solve 23251 - 스물셋 [java]

---
+++ baekjoon-online-judge/practice/23251.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        // Input
+        int T = Integer.parseInt(br.readLine());
+
+        while (T-- > 0) {
+            // Solve
+            sb.append(Integer.parseInt(br.readLine()) * 23).append("\n");
+        }
+
+        // Output
+        System.out.println(sb);
+    }
+}
