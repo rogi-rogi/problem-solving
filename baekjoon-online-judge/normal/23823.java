72004e834d5ecbb455535cf7524385f76ebd0c7b
rogi-rogi <yrkim6883@gmail.com>
2025-12-14 15:09:59 +0900

    boj: refactoring 23823 - 초코칩 케이크 [java]

--- baekjoon-online-judge/normal/23823.java
+++ baekjoon-online-judge/normal/23823.java
@@ -10,43 +10,36 @@ public class Main {
 		String[] in = br.readLine().split(" ");
 		int N = Integer.parseInt(in[0]);
 		int Q = Integer.parseInt(in[1]);
-		int[] rows = new int[N + 1], cols = new int[N + 1];
-		int rowCount = 0, colCount = 0;
+		int[] R = new int[N + 1], C = new int[N + 1];
 		int maxRow = 0, maxCol = 0;
+		int maxRowCount = N, maxColCount = N;
 
 		while (Q-- > 0) {
 			in = br.readLine().split(" ");
-			int t =  Integer.parseInt(in[0]);
-			int a =  Integer.parseInt(in[1]);
+			int t = Integer.parseInt(in[0]);
+			int a = Integer.parseInt(in[1]);
 
 			// Solve
 			if (t == 1) {
-				++rows[a];
-				if (rows[a] == maxRow) {
-					++rowCount;
-				} else if (rows[a] > maxRow) {
-					rowCount = 1;
+				++R[a];
+				if (R[a] == maxRow) {
+					++maxRowCount;
+				} else if (R[a] > maxRow) {
+					maxRowCount = 1;
+					maxRow = R[a];
 				}
-				maxRow = Math.max(maxRow, rows[a]);
 			} else {
-				++cols[a];
-				if (cols[a] == maxCol) {
-					++colCount;
-				} else if (cols[a] > maxCol) {
-					colCount = 1;
+				++C[a];
+				if (C[a] == maxCol) {
+					++maxColCount;
+				} else if (C[a] > maxCol) {
+					maxColCount = 1;
+					maxCol = C[a];
 				}
-				maxCol = Math.max(maxCol, cols[a]);
-			}
-
-			int result;
-			if (rowCount == 0 || colCount == 0) {
-				result = Math.max(rowCount, colCount) * N;
-			} else {
-				result = rowCount * colCount;
 			}
 
 			// Output
-			sb.append(result).append("\n");
+			sb.append(maxRowCount * maxColCount).append("\n");
 		}
 		System.out.println(sb);
 	}
