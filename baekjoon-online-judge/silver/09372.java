d77d7ea1dd103d50f1c8fb438fd6794ab63d7b0f
rogi-rogi <yrkim6883@gmail.com>
2026-02-21 11:05:28 +0900

    boj: refactor 16479 - 상근이의 여행 [java]

--- baekjoon-online-judge/silver/09372.java
+++ baekjoon-online-judge/silver/09372.java
@@ -1,25 +1,28 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
         StringBuilder sb = new StringBuilder();
 
         // Input
         int T = Integer.parseInt(br.readLine());
         while (T-- > 0) {
-            String[] input = br.readLine().split(" ");
-            final int N = Integer.parseInt(input[0]);
-            int M = Integer.parseInt(input[1]);
+            st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
             while (M-- > 0) {
                 br.readLine();
             }
 
             // Solve
-            sb.append(N - 1).append("\n");
+            sb.append(N - 1).append('\n');
         }
 
         // Output
-        System.out.println(sb);
+        System.out.print(sb);
     }
 }
