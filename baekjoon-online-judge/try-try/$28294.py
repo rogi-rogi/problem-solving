a54cb7d07a2bbd48a0d0a7c7402db21dcbb8141e
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2026-04-28 16:53:59 +0900

    chore: backup BOJ bronze/silver/gold solutions

--- baekjoon-online-judge/try-try/$28294.py
+++
@@ -1,7 +0,0 @@
-if __name__ == "__main__" :
-    N, a = map(int, input().split())
-    MOD = int(1e9+7)
-    res = (N-1)*pow(N-1,a-1, MOD)
-    A = pow(N, a-1, MOD)*(N-1)%MOD
-    R = (N-1)/N
-    print(int((res+(A-((1/N)*pow(N-1,a+1,MOD)))/(1-R))*N)%MOD)
